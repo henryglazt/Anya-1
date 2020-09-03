4c54c9949962236405fb0663094f7923c5617c6f
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-03 11:15:17 +0700

    Update and rename config.sample.js to config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 module.exports = {
 	/* The token of your Discord Bot */
-	token: "XXXXXXXXXXX",
+	token: "",
 	/* For the support server */
 	support: {
-		id: "XXXXXXXXXXX", // The ID of the support server
-		logs: "XXXXXXXXXXX", // And the ID of the logs channel of your server (new servers for example)
+		id: "339210008594087940", // The ID of the support server
+		logs: "736341595389165609", // And the ID of the logs channel of your server (new servers for example)
 	},
 	/* Dashboard configuration */
 	dashboard: {
@@ -16,52 +16,52 @@ module.exports = {
 		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
 		failureURL: "https://www.atlanta-bot.fr" // url on which users will be redirected if they click the cancel button (discord authentication)
 	},
-	mongoDB: "mongodb://localhost:27017/AtlantaBot", // The URl of the mongodb database
-	prefix: "*", // The default prefix for the bot
+	mongoDB: "mongodb+srv://nuruaruvi:laililham@nuruaruvi.nw8ve.azure.mongodb.net/nuruaruvi?retryWrites=true&w=majority", // The URl of the mongodb database
+	prefix: "?!", // The default prefix for the bot
 	/* For the embeds (embeded messages) */
 	embed: {
-		color: "#0091fc", // The default color for the embeds
-		footer: "Atlanta | Open Source" // And the default footer for the embeds
+		color: "#8300ff", // The default color for the embeds
+		footer: "NuruAruvi" // And the default footer for the embeds
 	},
 	/* Bot's owner informations */
 	owner: {
-		id: "XXXXXXXXXXX", // The ID of the bot's owner
-		name: "Androz#2091" // And the name of the bot's owner
+		id: "300944075983421451", // The ID of the bot's owner
+		name: "DooM™#6932" // And the name of the bot's owner
 	},
 	/* DBL votes webhook (optional) */
 	votes: {
 		port: 5000, // The port for the server
-		password: "XXXXXXXXXXX", // The webhook auth that you have defined on discordbots.org
-		channel: "XXXXXXXXXXX" // The ID of the channel that in you want the votes logs
+		password: "", // The webhook auth that you have defined on discordbots.org
+		channel: "" // The ID of the channel that in you want the votes logs
 	},
 	/* The API keys that are required for certain commands */
 	apiKeys: {
 		// BLAGUE.XYZ: https://blague.xyz/
-		blagueXYZ: "XXXXXXXXXXX",
+		blagueXYZ: "",
 		// FORTNITE TRN: https://fortnitetracker.com/site-api
-		fortniteTRN: "XXXXXXXXXXX",
+		fortniteTRN: "",
 		// FORTNITE FNBR: https://fnbr.co/api/docs
-		fortniteFNBR: "XXXXXXXXXXX",
+		fortniteFNBR: "",
 		// DBL: https://discordbots.org/api/docs#mybots
-		dbl: "XXXXXXXXXXX",
+		dbl: "",
 		// AMETHYSTE: https://api.amethyste.moe
-		amethyste: "XXXXXXXXXXX",
+		amethyste: "",
 		// SENTRY: https://sentry.io (this is not required and not recommended)
-		sentryDSN: "XXXXXXXXXXX"
+		sentryDSN: ""
 	},
 	/* The others utils links */
 	others: {
-		github: "https://github.com/Androz2091", // Founder's github account
-		donate: "https://patreon.com/Androz2091" // Donate link
+		github: "", // Founder's github account
+		donate: "" // Donate link
 	},
 	/* The Bot status */
 	status: [
 		{
-			name: "@Atlanta help on {serversCount} servers",
+			name: "My own voice",
 			type: "LISTENING"
 		},
 		{
-			name: "my website : atlanta-bot.fr",
+			name: "And making love",
 			type: "PLAYING"
 		}
 	],
