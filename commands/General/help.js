d0c90ea705222838bb5db8007fc25cc0de6278bb
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-04 18:02:27 +0700

    Update help.js

--- commands/General/help.js
+++ commands/General/help.js
@@ -121,11 +121,8 @@ class Help extends Command {
 		}
         
 		embed.addField("\u200B", message.translate("misc:STATS_FOOTER", {
-			donateLink: "https://patreon.com/Androz2091",
-			dashboardLink: "https://dashboard.atlanta-bot.fr",
 			inviteLink: await this.client.generateInvite("ADMINISTRATOR"),
-			githubLink: "https://github.com/Androz2091",
-			supportLink: "https://discord.atlanta-bot.fr"
+			supportLink: "https://discord.gg/gangsebelah"
 		}));
 		embed.setAuthor(message.translate("general/help:TITLE", {
 			name: this.client.user.username
