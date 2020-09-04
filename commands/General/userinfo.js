fde5e52574c0c6c6cb9049ff3339eaf8c0413b4e
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-05 06:57:02 +0700

    Update userinfo.js

--- commands/General/userinfo.js
+++ commands/General/userinfo.js
@@ -101,9 +101,9 @@ class Userinfo extends Command {
 		.map(role => role.toString())
 		.slice(0, -1);
 			embed.addField("**❯ Member:**", [
-			`**● Nickname:** ${member.nickname ? member.nickname : message.translate("general/userinfo:NO_NICKNAME")}`,
+			`**● Nickname:** ${member.nickname ? member.nickname : \`message.translate("general/userinfo:NO_NICKNAME")\`}`,
 			`**● Highest Role:** ${member.roles.highest ? member.roles.highest : message.translate("general/userinfo:NO_ROLE")}`,
-			`**● Hex Color:** \`${color}\``,
+			`**● Hex Color:** \`${member.displayHexColor}\``,
 			`**● Roles [${roles.length}]:** ${roles.length < 16 && roles.length !== 0 ? roles.join(', ') : roles.length > 15 ? ('Too Many Roles!') : '`None`'}`,
 			`**● Joined Date:** ${message.printDate(member.joinedAt)}`,
 			`**● Acknowledgements:** ${member.guild.owner.id === member.user.id ? ('Server Owner') : member.hasPermission('ADMINISTRATOR') ? ('Server Admin') : '`None`'}`,
