ad33c5d0993800f67d62dcd008fe9af43475079c
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-05 15:49:41 +0700

    Update work.js

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -49,13 +49,13 @@ class Work extends Command {
 			.setFooter(message.translate("economy/work:AWARD"), message.author.displayAvatarURL())
 			.setColor(data.config.embed.color);
         
-		const award = {
+		const award = [
 			this.client.customEmojis.letters.a,
 			this.client.customEmojis.letters.w,
 			this.client.customEmojis.letters.a,
 			this.client.customEmojis.letters.r,
 			this.client.customEmojis.letters.d
-		};
+		];
 		let won = 200;
 
 		if(data.memberData.workStreak >= 5){
