d31039733cdc5d025ef36ab1162186cce5147785
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-05 17:16:59 +0700

    Update work.js

--- commands/Economy/work.js
+++ commands/Economy/work.js
@@ -69,7 +69,7 @@ class Work extends Command {
 			for(let i = 0; i < award.length; i++){
 				if(data.memberData.workStreak > i){
 					let letter = Discord.Util.parseEmoji(award[i]).name.split("_")[1];
-					award[i] = `:regional_indicator_${letter.toLowerCase()}:`;
+					award[i] = `:regional_indicator_${letter}:`;
 				}
 			}
 			embed.addField(message.translate("economy/work:SALARY"), message.translate("economy/work:SALARY_CONTENT", {
