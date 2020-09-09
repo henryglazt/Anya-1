7fb65c77c977e764f6b8f9a0c7c0d81fdd231871
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-09 14:31:22 +0700

    Update shuffle.js

--- commands/Music/shuffle.js
+++ commands/Music/shuffle.js
@@ -29,7 +29,7 @@ class Shuffle extends Command {
 			return message.error("music/play:MY_VOICE_CHANNEL");
 		}
 
-		if(!this.client.player.isPlaying(message.guild.id))
+		if(!this.client.player.isPlaying(message.guild.id)) {
 			return message.error("music/play:NOT_PLAYING");
 		}
 
