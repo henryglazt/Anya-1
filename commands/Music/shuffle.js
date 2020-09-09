a8cfdead16d1de8fdb44e2404e0f17de63c07de4
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-09 13:15:35 +0700

    Create shuffle.js

---
+++ commands/Music/shuffle.js
@@ -0,0 +1,41 @@
+const Command = require("../../base/Command.js"),
+	Discord = require("discord.js");
+
+class Shuffle extends Command {
+
+	constructor (client) {
+		super(client, {
+			name: "shuffle",
+			dirname: __dirname,
+			enabled: true,
+			guildOnly: true,
+			aliases: [ "acak" ],
+			memberPermissions: [],
+			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+			nsfw: false,
+			ownerOnly: false,
+			cooldown: 5000
+		});
+	}
+
+	async run (message, args, data) {
+
+		const voice = message.member.voice.channel;
+		if (!voice){
+			return message.error("music/play:NO_VOICE_CHANNEL");
+		}
+        
+		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+			return message.error("music/play:MY_VOICE_CHANNEL");
+		}
+
+		if(!client.player.isPlaying(message.guild.id))
+			return message.error("music/play:NOT_PLAYING");
+		}
+
+		await this.client.player.shuffle(message.guild.id);
+
+		message.sendT("music/shuffle:SUCCESS");
+
+
+module.exports = Shuffle;
