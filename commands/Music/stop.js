875e059266e3dce2915c07f31baada0b2b9d6edd
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-13 05:33:00 +0700

    Update stop.js

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -1,79 +1,65 @@
 const Command = require("../../base/Command.js"),
 	Discord = require("discord.js");
-
 class Stop extends Command {
-
-	constructor (client) {
+	constructor(client) {
 		super(client, {
 			name: "stop",
 			dirname: __dirname,
 			enabled: true,
 			guildOnly: true,
-			aliases: [ "dc", "leave", "disconnect" ],
+			aliases: ["dc", "leave", "disconnect"],
 			memberPermissions: [],
-			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+			botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
 			nsfw: false,
 			ownerOnly: false,
 			cooldown: 5000
 		});
 	}
-
-	async run (message, args, data) {
-
+	async run(message, args, data) {
 		const voice = message.member.voice.channel;
-		if (!voice){
+		if(!voice) {
 			return message.error("music/play:NO_VOICE_CHANNEL");
 		}
-        
-		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+		if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
 			return message.error("music/play:MY_VOICE_CHANNEL");
 		}
-
 		if(!this.client.distube.isPlaying(message)) {
 			return message.error("music/play:NOT_PLAYING");
 		}
-
 		const members = voice.members.filter((m) => !m.user.bot);
-
-		const embed = new Discord.MessageEmbed()
-			.setAuthor(message.translate("music/stop:DESCRIPTION"))
-			.setFooter(data.config.embed.footer)
-			.setColor(data.config.embed.color);
-
+		const embed = new Discord.MessageEmbed().setAuthor(message.translate("music/stop:DESCRIPTION")).setFooter(data.config.embed.footer).setColor(data.config.embed.color);
 		const m = await message.channel.send(embed);
-
-		if(members.size > 1){
-            
+		if(members.size > 1) {
 			m.react("⏹️");
-
-			const mustVote = Math.floor(members.size/2+1);
-
+			const mustVote = Math.floor(members.size / 2 + 1);
 			embed.setDescription(message.translate("music/stop:VOTE_CONTENT", {
 				voteCount: 0,
 				requiredCount: mustVote
 			}));
 			m.edit(embed);
-    
 			const filter = (reaction, user) => {
 				const member = message.guild.members.cache.get(user.id);
 				const voiceChannel = member.voice.channel;
-				if(voiceChannel){
+				if(voiceChannel) {
 					return voiceChannel.id === voice.id;
 				}
 			};
-
 			const collector = await m.createReactionCollector(filter, {
 				time: 25000
 			});
-
 			collector.on("collect", (reaction) => {
-				const haveVoted = reaction.count-1;
-				if(haveVoted >= mustVote){
+				const haveVoted = reaction.count - 1;
+				if(haveVoted >= mustVote) {
 					this.client.distube.stop(message);
-					message.channel.send({embed: {
-            color: data.config.embed.color, footer: {text: data.config.embed.footer},
-            description: message.translate("music/stop:SUCCESS")
-          }});
+					message.channel.send({
+						embed: {
+							color: data.config.embed.color,
+							footer: {
+								text: data.config.embed.footer
+							},
+							description: message.translate("music/stop:SUCCESS")
+						}
+					});
 					collector.stop(true);
 				} else {
 					embed.setDescription(message.translate("music/stop:VOTE_CONTENT", {
@@ -83,12 +69,14 @@ class Stop extends Command {
 					m.edit(embed);
 				}
 			});
-
 			collector.on("end", (collected, isDone) => {
-        return m.delete(embed);
+				return m.delete(embed);
 			});
-    }
+		} else {
+			this.client.distube.stop(message);
+			embed.setDescription(message.translate("music/stop:SUCCESS"));
+			m.edit(embed);
+		}
 	}
 }
-
 module.exports = Stop;
