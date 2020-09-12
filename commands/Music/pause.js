2199c89c2d3efad72fc70c8aca82b5ddbb6582a9
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-12 22:56:28 +0700

    Update pause.js

--- commands/Music/pause.js
+++ commands/Music/pause.js
@@ -19,27 +19,30 @@ class Pause extends Command {
 
 	async run (message) {
 
-		const queue = this.client.player.getQueue(message.guild.id);
-
 		const voice = message.member.voice.channel;
 		if (!voice){
 			return message.error("music/play:NO_VOICE_CHANNEL");
 		}
+        
+		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+			return message.error("music/play:MY_VOICE_CHANNEL");
+		}
 
-		if(!queue){
-			return message.error("music:play:NOT_PLAYING");
+		if(!this.client.distube.isPlaying(message)) {
+			return message.error("music/play:NOT_PLAYING");
+		}
+
+		if(this.client.distube.isPaused(message)) {
+			return message.error("music/pause:PAUSED");
 		}
 
 		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
 			return message.error("music/play:MY_VOICE_CHANNEL");
 		}
 
-		// Gets the current song
-		await this.client.player.pause(message.guild.id);
-        
-		// Send the embed in the current channel
+		this.client.distube.pause(message);
+
 		message.sendT("music/pause:SUCCESS");
-        
 	}
 
 }
