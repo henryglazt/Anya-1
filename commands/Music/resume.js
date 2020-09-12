13a92e06175595d44c882387f4f7be74f4775727
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-12 20:16:32 +0700

    Update resume.js

--- commands/Music/resume.js
+++ commands/Music/resume.js
@@ -19,25 +19,25 @@ class Resume extends Command {
 
 	async run (message) {
 
-		const queue = this.client.player.getQueue(message.guild.id);
-
 		const voice = message.member.voice.channel;
 		if (!voice){
 			return message.error("music/play:NO_VOICE_CHANNEL");
 		}
+        
+		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+			return message.error("music/play:MY_VOICE_CHANNEL");
+		}
 
-		if(!queue){
-			return message.error("music:play:NOT_PLAYING");
+		if(!this.client.distube.isPlaying(message)) {
+			return message.error("music/play:NOT_PLAYING");
 		}
 
 		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
 			return message.error("music/play:MY_VOICE_CHANNEL");
 		}
 
-		// Gets the current song
-		await this.client.player.resume(message.guild.id);
-        
-		// Send the embed in the current channel
+		this.client.distube.resume(message);
+
 		message.sendT("music/resume:SUCCESS");
 	}
 
