c49f58ef586bac84be31d380215746eb6a982d99
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-13 06:09:05 +0700

    Update repeat.js

--- commands/Music/repeat.js
+++ commands/Music/repeat.js
@@ -1,54 +1,54 @@
 const Command = require("../../base/Command.js"),
 	Discord = require("discord.js");
-
-class Repeat extends Command {
-
-	constructor (client) {
+class Loop extends Command {
+	constructor(client) {
 		super(client, {
-			name: "repeat",
+			name: "loop",
 			dirname: __dirname,
 			enabled: true,
 			guildOnly: true,
-			aliases: [ "loop", "ulang" ],
+			aliases: ["repeat", "ulang"],
 			memberPermissions: [],
-			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+			botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
 			nsfw: false,
 			ownerOnly: false,
 			cooldown: 5000
 		});
 	}
-
-	async run (message, args, data) {
-
+	async run(message, args, data) {
 		const voice = message.member.voice.channel;
-		if (!voice){
+		if(!voice) {
 			return message.error("music/play:NO_VOICE_CHANNEL");
 		}
-        
-		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+		if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
 			return message.error("music/play:MY_VOICE_CHANNEL");
 		}
-
 		if(!this.client.distube.isPlaying(message)) {
 			return message.error("music/play:NOT_PLAYING");
 		}
-
 		let mode = null;
-		switch (args[0]) {
-		case "off":
-		mode = 0
-		break
-		case "song":
-		mode = 1
-		break
-		case "queue":
-		mode = 2
-		break
+		switch(args[0]) {
+			case "off":
+				mode = 0
+				break
+			case "song":
+				mode = 1
+				break
+			case "queue":
+				mode = 2
+				break
 		}
 		mode = this.client.distube.setRepeatMode(message, mode);
-		mode = mode ? mode == 2 ? "Repeat queue" : "Repeat song" : "Off";
-		message.success("music/shuffle:MODE");
-		}
+		mode = mode ? mode == 2 ? message.translate("music/loop:SUCCESS_QUEUE") : message.translate("music/loop:SUCCESS_SONG") : message.translate("music/loop:OFF");
+		message.channel.send({
+			embed: {
+				color: data.config.embed.color,
+				footer: {
+					text: data.config.embed.footer
+				},
+				description: mode
+			}
+		});
 	}
-
-module.exports = Repeat;
+}
+module.exports = Loop;
