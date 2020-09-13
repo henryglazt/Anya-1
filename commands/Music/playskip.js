7fe525ecd16fc6c767ed74e2f2d108d966dd9e36
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-13 20:33:13 +0700

    Create playskip.js

---
+++ commands/Music/playskip.js
@@ -0,0 +1,29 @@
+const Command = require("../../base/Command.js"),
+	Discord = require("discord.js");
+class Playskip extends Command {
+	constructor(client) {
+		super(client, {
+			name: "playskip",
+			dirname: __dirname,
+			enabled: true,
+			guildOnly: true,
+			aliases: ["insert"],
+			memberPermissions: [],
+			botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+			nsfw: false,
+			ownerOnly: false,
+			cooldown: 5000
+		});
+	}
+	async run(message, args, data) {
+		const voice = message.member.voice.channel;
+		if(!voice) {
+			return message.error("music/play:NO_VOICE_CHANNEL");
+		}
+		if(message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+			return message.error("music/play:MY_VOICE_CHANNEL");
+		}
+		if(!this.client.distube.isPlaying(message)) {
+			return message.error("music/play:NOT_PLAYING");
+		}
+module.exports = Playskip;
