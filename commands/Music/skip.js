3e042a1872ecb9db99d90a02e12f8013211d5568
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-13 13:54:17 +0700

    Update skip.js

--- commands/Music/skip.js
+++ commands/Music/skip.js
@@ -7,7 +7,7 @@ class Skip extends Command {
 			dirname: __dirname,
 			enabled: true,
 			guildOnly: true,
-			aliases: ["next"],
+			aliases: ["next", "s", "lanjut"],
 			memberPermissions: [],
 			botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
 			nsfw: false,
