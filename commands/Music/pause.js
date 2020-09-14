c85f79dd455d6fd975630424e18523f2cb6c2ffe
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-15 04:16:49 +0700

    Update pause.js

--- commands/Music/pause.js
+++ commands/Music/pause.js
@@ -2,51 +2,52 @@ const Command = require("../../base/Command.js");
 
 class Pause extends Command {
 
-	constructor (client) {
-		super(client, {
-			name: "pause",
-			dirname: __dirname,
-			enabled: true,
-			guildOnly: true,
-			aliases: [],
-			memberPermissions: [],
-			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-			nsfw: false,
-			ownerOnly: false,
-			cooldown: 5000
-		});
-	}
+    constructor(client) {
+        super(client, {
+            name: "pause",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: [],
+            memberPermissions: [],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 5000
+        });
+    }
 
-	async run (message) {
+    async run(message) {
 
-		const voice = message.member.voice.channel;
-		if (!voice){
-			return message.error("music/play:NO_VOICE_CHANNEL");
-		}
-        
-		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-			return message.error("music/play:MY_VOICE_CHANNEL");
-		}
+        const voice = message.member.voice.channel;
+        if (!voice) {
+            return message.error("music/play:NO_VOICE_CHANNEL");
+        }
 
-		if(!this.client.distube.isPlaying(message)) {
-			return message.error("music/play:NOT_PLAYING");
-		}
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+            return message.error("music/play:MY_VOICE_CHANNEL");
+        }
 
-		if(this.client.distube.isPaused(message)) {
-			return message.error("music/pause:PAUSED");
-		}
+        if (!this.client.distube.isPlaying(message)) {
+            return message.error("music/play:NOT_PLAYING");
+        }
 
-		this.client.distube.pause(message);
+        if (this.client.distube.isPaused(message)) {
+            return message.error("music/pause:PAUSED");
+        }
 
-		message.channel.send({
-			embed: {
-				color: data.config.embed.color,
-				footer: {
-					text: data.config.embed.footer
-				},
-				description: message.translate("music/pause:SUCCESS")
-			}
-		});
-	}
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+            return message.error("music/play:MY_VOICE_CHANNEL");
+        }
+
+        const queue = this.client.distube.getQueue(message);
+        if (queue.dispatcher.paused) {
+            message.error("music/pause:PAUSED");
+        } else {
+            queue.dispatcher.pause();
+            message.sendT("music/pause:SUCCESS");
+        }
+    }
 }
+
 module.exports = Pause;
