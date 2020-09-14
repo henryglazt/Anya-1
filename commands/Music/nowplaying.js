b9e941d9dc4b3e0c669f8173d5fdc8cf18d9cf5c
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-15 04:14:35 +0700

    Update nowplaying.js

--- commands/Music/nowplaying.js
+++ commands/Music/nowplaying.js
@@ -1,55 +1,58 @@
 const Command = require("../../base/Command.js"),
-	Discord = require("discord.js");
+    Discord = require("discord.js"),
+    createBar = require("string-progressbar");
 
 class Nowplaying extends Command {
 
-	constructor (client) {
-		super(client, {
-			name: "nowplaying",
-			dirname: __dirname,
-			enabled: true,
-			guildOnly: true,
-			aliases: [ "np" ],
-			memberPermissions: [],
-			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-			nsfw: false,
-			ownerOnly: false,
-			cooldown: 5000
-		});
-	}
+    constructor(client) {
+        super(client, {
+            name: "nowplaying",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: ["np"],
+            memberPermissions: [],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 5000
+        });
+    }
 
-	async run (message, args, data) {
+    async run(message, args, data) {
 
-		const queue = this.client.distube.getQueue(message);
+        const queue = this.client.distube.getQueue(message);
+        const voice = message.member.voice.channel;
+        if (!voice) {
+            return message.error("music/play:NO_VOICE_CHANNEL");
+        }
 
-		const voice = message.member.voice.channel;
-		if (!voice){
-			return message.error("music/play:NO_VOICE_CHANNEL");
-		}
+        if (!queue) {
+            return message.error("music/play:NOT_PLAYING");
+        }
 
-		if(!queue){
-			return message.error("music/play:NOT_PLAYING");
-		}
+        const song = queue.songs[0];
+        const seek = (queue.dispatcher.streamTime - queue.dispatcher.pausedTime) / 1000;
+        const left = song.duration - seek;
 
-		// Gets the current song
-		const track = await this.client.distube.isPlaying(message);
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(message.translate("music/np:NOW_PLAYING"), "https://cdn.discordapp.com/emojis/750364941449691206.gif")
+            .setThumbnail(queue.songs[0].thumbnail)
+            .addField(message.translate("music/np:TITLE"), "[" + queue.songs[0].name + "](" + queue.songs[0].url + ")")
+            .addField(
+                message.translate("music/np:ELAPSED"),
+                new Date(seek * 1000)
+                .toISOString()
+                .substr(11, 8) + "\n" +
+                "`" + createBar(song.duration == 0 ? seek : song.duration, seek, 16)[0] + "`" + "\n" + (song.duration == 0 ? " ◉ LIVE" : new Date(song.duration * 1000)
+                    .toISOString()
+                    .substr(11, 8))
+            )
+            .setColor(data.config.embed.color)
+            .setFooter(data.config.embed.footer);
+        message.channel.send(embed);
 
-		// Generate discord embed to display song informations
-		const embed = new Discord.MessageEmbed()
-			.setAuthor(message.translate("music/np:CURRENTLY_PLAYING"))
-			.setThumbnail(track.thumbnail)
-			.addField(message.translate("music/np:T_TITLE"), track.name, true)
-			.addField(message.translate("music/np:T_CHANNEL"), track.author, true)
-			.addField(message.translate("music/np:T_DURATION"), message.convertTime(Date.now()+track.duration, "to", true), true)
-			.setTimestamp()
-			.setColor(data.config.embed.color)
-			.setFooter(data.config.embed.footer);
-        
-		// Send the embed in the current channel
-		message.channel.send(embed);
-        
-	}
-
+    }
 }
 
 module.exports = Nowplaying;
