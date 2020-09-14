7916e0134177341914e6c8224996b16ce93f904c
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-15 05:28:50 +0700

    Update stop.js

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -87,7 +87,7 @@ class Stop extends Command {
                 }
             });
             collector.on("end", (collected, isDone) => {
-                return m.delete(embed);
+                return m.reactions.removeAll();
             });
         } else {
             this.client.distube.stop(message);
