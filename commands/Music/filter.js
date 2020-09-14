2a8596acb2ebace1690f5a087d01f40362ef6ef5
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-14 17:33:52 +0700

    Update filter.js

--- commands/Music/filter.js
+++ commands/Music/filter.js
@@ -34,29 +34,10 @@ class Filter extends Command {
 			return message.error("music/play:NOT_PLAYING");
 		}
 
-		const filter = args[0];
-		if(!filter) return message.channel.send(`Please specify a valid filter to enable or disable`);
-
-		const filterToUpdate = Object.values(filters).find((f) => f.toLowerCase() === filter.toLowerCase());
-
-		if(!filterToUpdate) return message.channel.send(`This filter doesn't exist`);
-
-		const filterRealName = Object.keys(filters).find((f) => filters[f] === filterToUpdate);
-
-		const queueFilters = this.client.player.getQueue(message.guild.id).filters
-		const filtersUpdated = {};
-		filtersUpdated[filterRealName] = queueFilters[filterRealName] ? false : true;
-		this.client.player.setFilters(message.guild.id, filtersUpdated);
-
-		if(filtersUpdated[filterRealName]) {
-
-		message.channel.send(`I'm adding the filter to the music, please wait... Note : the longer the music is, the longer the wait will be`);
-
-		} else {
-
-		message.channel.send(`I'm disabling the filter on the music, please wait... Note : the longer the music is playing, the longer the wait will be`);
+		if ([`3d`, `bassboost`, `echo`, `flanger`, `gate`, `haas`, `karaoke`, `nightcore`, `reverse`, `vaporwave`].includes(args[0])) {
+		let filter = client.distube.setFilter(message, args[0]);
+		message.channel.send("Current queue filter: " + (filter || "Off"));
 		}
-	}
 }
 
 module.exports = Filter;
