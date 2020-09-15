b1c1b749f6cb2ce928eeefc6cc1b5f6855bb66dc
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-16 02:27:10 +0700

    Update queue.js

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -1,53 +1,127 @@
 const Command = require("../../base/Command.js"),
-	Discord = require("discord.js");
+    Discord = require("discord.js");
 
 class Queue extends Command {
 
-	constructor (client) {
-		super(client, {
-			name: "queue",
-			dirname: __dirname,
-			enabled: true,
-			guildOnly: true,
-			aliases: [ "q" ],
-			memberPermissions: [],
-			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-			nsfw: false,
-			ownerOnly: false,
-			cooldown: 5000
-		});
-	}
+    constructor(client) {
+        super(client, {
+            name: "queue",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: ["q"],
+            memberPermissions: [],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 5000
+        });
+    }
 
-	async run (message, args, data) {
+    async run(message, args, data) {
 
-		const voice = message.member.voice.channel;
-		if (!voice){
-			return message.error("music/play:NO_VOICE_CHANNEL");
-		}
-        
-		const queue = this.client.player.getQueue(message.guild.id);
+        const queue = this.client.distube.getQueue(message.guild.id);
 
-		if(!queue){
-			return message.error("music:play:NOT_PLAYING");
-		}
+        if (!queue) {
+            return message.error("music/play:NOT_PLAYING");
+        }
+        const q = queue.songs.slice(1)
+            .map((song, i) => {
+                return `**${i+1}.** [${song.name}](${song.url}) - \`${song.duration == 0 ? "◉ LIVE" : song.formattedDuration}\` - ${song.user}`
+            });
 
-		const tracks = [ ...[ queue.playing ], ...queue.tracks ];
-		if(tracks.length > 20) tracks.splice(20);
-		const sQueue = tracks.map((track, i) => {
-			return `${i+1 ? "\nCurrently playing...\n" : ""} **${message.translate("common:TITLE")}**: ${track.name}\n**${message.translate("common:REQUESTEDBY")}**: ${track.requestedBy}`;
-		});
+        let i0 = 0;
+        let i1 = 10;
+        let page = 1;
 
-		// Generate discord embed to display the songs list
-		const embed = new Discord.MessageEmbed()
-			.addField(this.client.customEmojis.playlist+" "+message.translate("music/queue:TITLE"), sQueue.join("\n-------\n"))
-			.setColor(data.config.embed.color)
-			.setFooter(data.config.embed.footer)
-			.setTimestamp();
-        
-		// Then, send the embed in the current channel
-		message.channel.send(embed);
-        
-	}
+        const current = queue.songs[0];
+        let description = `[${current.name}](${current.url}) - \`${current.duration == 0 ? "◉ LIVE" : current.formattedDuration}\` - ${current.user}\n\`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\`\n`;
+        description = description += q.slice(0, 10)
+            .join("\n");
+
+        const embed = new Discord.MessageEmbed()
+            .setAuthor(message.translate("music/queue:TITLE"), "https://cdn.discordapp.com/emojis/598167786195517440.png")
+            .setTitle(message.translate("music/np:NOW_PLAYING"))
+            .setColor(data.config.embed.color)
+            .setThumbnail(current.thumbnail)
+            .setFooter(`${message.translate("common:PAGE")}: ${page}/${Math.ceil(queue.songs.length/10)}`)
+            .setDescription(description);
+
+        const msg = await message.channel.send(embed);
+
+        await msg.react("⬅️");
+        await msg.react("➡️");
+        await msg.react("❌");
+
+        const collector = msg.createReactionCollector((reaction, user) => user.id === message.author.id);
+
+        collector.on("collect", async(reaction) => {
+
+            if (reaction._emoji.name === "⬅️") {
+
+                // Updates variables
+                i0 = i0 - 10;
+                i1 = i1 - 10;
+                page = page - 1;
+
+                // if there is no guild to display, delete the message
+                if (i0 < 0) {
+                    return msg.delete();
+                }
+                if (!i0 || !i1) {
+                    return msg.delete();
+                }
+
+                let description = `[${current.name}](${current.url}) - \`${current.duration == 0 ? "◉ LIVE" : current.formattedDuration}\` - ${current.user}\n\`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\`\n`;
+                description = description += q.slice(i0, i1)
+                    .join("\n");
+
+                // Update the embed with new informations
+                embed.setFooter(`${message.translate("common:PAGE")}: ${page}/${Math.round(queue.songs.length/10)}`)
+                    .setDescription(description);
+
+                // Edit the message 
+                msg.edit(embed);
+
+            }
+
+            if (reaction._emoji.name === "➡️") {
+
+                // Updates variables
+                i0 = i0 + 10;
+                i1 = i1 + 10;
+                page = page + 1;
+
+                // if there is no guild to display, delete the message
+                if (i1 > queue.songs.length + 10) {
+                    return msg.delete();
+                }
+                if (!i0 || !i1) {
+                    return msg.delete();
+                }
+
+                let description = `[${current.name}](${current.url}) - \`${current.duration == 0 ? "◉ LIVE" : current.formattedDuration}\` - ${current.user}\n\`▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\`\n`;
+                description = description += q.slice(i0, i1)
+                    .join("\n");
+
+                // Update the embed with new informations
+                embed.setFooter(`${message.translate("common:PAGE")}: ${page}/${Math.round(queue.songs.length/10)}`)
+                    .setDescription(description);
+
+                // Edit the message 
+                msg.edit(embed);
+
+            }
+
+            if (reaction._emoji.name === "❌") {
+                return msg.delete();
+            }
+
+            // Remove the reaction when the user react to the message
+            await reaction.users.remove(message.author.id);
+
+        });
+    }
 
 }
 
