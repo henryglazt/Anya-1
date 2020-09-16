8f422d6c8ccd1ffdf8f81faa634b59a2d9bc0f83
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-16 22:18:21 +0700

    Update volume.js

--- commands/Music/volume.js
+++ commands/Music/volume.js
@@ -1,65 +1,59 @@
 const Command = require("../../base/Command.js"),
-	Discord = require("discord.js");
-
+    Discord = require("discord.js");
 class Volume extends Command {
-
-	constructor (client) {
-		super(client, {
-			name: "volume",
-			dirname: __dirname,
-			enabled: true,
-			guildOnly: true,
-			aliases: [ "vol" ],
-			memberPermissions: [],
-			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
-			nsfw: false,
-			ownerOnly: false,
-			cooldown: 5000
-		});
-	}
-
-	async run (message, args, data) {
-		const embed = new Discord.MessageEmbed()
-		.setColor(data.config.embed.color)
-		.setFooter(data.config.embed.footer)
-
-		const voice = message.member.voice.channel;
-		if (!voice){
-			return message.error("music/play:NO_VOICE_CHANNEL");
-		}
-		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-			return message.error("music/play:MY_VOICE_CHANNEL");
-		}
-		if (!this.client.distube.isPlaying(message)) {
-			return message.error("music/play:NOT_PLAYING");
-		}
-		let volume = parseInt(args[0])
-		if(!args[0]){
-			embed.setDescription(message.translate("music/volume:EXAMPLES"));
-			return message.channel.send(embed);
-		}
-		if(isNaN(volume)){
-			embed.setDescription(message.translate("music/volume:VALUE"));
-			return message.channel.send(embed);
-		}
-		if(200 < volume){
-			embed.setDescription(message.translate("music/volume:VALUE"));
-			return message.channel.send(embed);
-		}
-		if(volume <=0){
-			embed.setDescription(message.translate("music/volume:VALUE"));
-			return message.channel.send(embed);
-		}
-		this.client.distube.setVolume(message, volume);
-		message.channel.send({
-			embed: {
-				color: data.config.embed.color,
-				footer: {
-					text: data.config.embed.footer
-				},
-				description: message.translate("music/volume:SUCCESS", {volume: volume})
-			}
-		});
-	}
+    constructor(client) {
+        super(client, {
+            name: "volume",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: ["vol"],
+            memberPermissions: [],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 5000
+        });
+    }
+    async run(message, args, data) {
+        const embed = new Discord.MessageEmbed()
+            .setColor(data.config.embed.color)
+            .setFooter(data.config.embed.footer)
+        const voice = message.member.voice.channel;
+        if (!voice) {
+            embed.setDescription(message.translate("music/play:NO_VOICE_CHANNEL"));
+            return message.channel.send(embed);
+        }
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+            embed.setDescription(message.translate("music/play:MY_VOICE_CHANNEL"));
+            return message.channel.send(embed);
+        }
+        if (!this.client.distube.isPlaying(message)) {
+            embed.setDescription(message.translate("music/play:NOT_PLAYING"));
+            return message.channel.send(embed);
+        }
+        let volume = parseInt(args[0])
+        if (!args[0]) {
+            embed.setDescription(message.translate("music/volume:EXAMPLES"));
+            return message.channel.send(embed);
+        }
+        if (isNaN(volume)) {
+            embed.setDescription(message.translate("music/volume:VALUE"));
+            return message.channel.send(embed);
+        }
+        if (200 < volume) {
+            embed.setDescription(message.translate("music/volume:VALUE"));
+            return message.channel.send(embed);
+        }
+        if (volume <= 0) {
+            embed.setDescription(message.translate("music/volume:VALUE"));
+            return message.channel.send(embed);
+        }
+        this.client.distube.setVolume(message, volume);
+        embed.setDescription(message.translate("music/volume:SUCCESS", {
+            volume: volume
+        }));
+        return message.channel.send(embed);
+    }
 }
 module.exports = Volume;
