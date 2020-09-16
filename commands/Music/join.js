de0758141e627207af43239c74918a2ddabba745
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-16 23:29:39 +0700

    Update join.js

--- commands/Music/join.js
+++ commands/Music/join.js
@@ -1,47 +1,48 @@
 const Command = require("../../base/Command.js"),
-	Discord = require("discord.js");
+    Discord = require("discord.js");
 class Join extends Command {
-	constructor(client) {
-		super(client, {
-			name: "join",
-			dirname: __dirname,
-			enabled: true,
-			guildOnly: true,
-			aliases: ["summon", "masuk"],
-			memberPermissions: [],
-			botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
-			nsfw: false,
-			ownerOnly: false,
-			cooldown: 5000
-		});
-	}
-	async run(message, args, data) {
-		const voice = message.member.voice.channel;
-		if(!voice) {
-			return message.error("music/play:NO_VOICE_CHANNEL");
-		}
-		if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-		return message.error("music/play:MY_VOICE_CHANNEL");
-		}
-		if(message.guild.me.voice.channel && message.member.voice.channel.id === message.guild.me.voice.channel.id) {
-			return message.error("music/play:JOINED");
-		}
-		const perms = voice.permissionsFor(this.client.user);
-		if(!perms.has("CONNECT") || !perms.has("SPEAK")) {
-			return message.error("music/play:VOICE_CHANNEL_CONNECT");
-		}
-		voice.join().then(connection => {
-			connection.voice.setSelfDeaf(true)
-			message.channel.send({
-				embed: {
-					color: data.config.embed.color,
-					footer: {
-						text: data.config.embed.footer
-					},
-					description: this.client.customEmojis.success + " | " + message.translate("music/play:JOIN")
-				}
-			})
-		});
-	}
+    constructor(client) {
+        super(client, {
+            name: "join",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: ["summon", "masuk"],
+            memberPermissions: [],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 5000
+        });
+    }
+    async run(message, args, data) {
+        const xembed = new Discord.MessageEmbed()
+            .setColor(data.config.embed.color)
+            .setFooter(data.config.embed.footer)
+        const voice = message.member.voice.channel;
+        if (!voice) {
+            xembed.setDescription(message.translate("music/play:NO_VOICE_CHANNEL"));
+            return message.channel.send(xembed);
+        }
+        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
+            xembed.setDescription(message.translate("music/play:MY_VOICE_CHANNEL"));
+            return message.channel.send(xembed);
+        }
+        if (message.guild.me.voice.channel && message.member.voice.channel.id === message.guild.me.voice.channel.id) {
+            xembed.setDescription(message.translate("music/play:JOINED"));
+            return message.channel.send(xembed);
+        }
+        const perms = voice.permissionsFor(this.client.user);
+        if (!perms.has("CONNECT") || !perms.has("SPEAK")) {
+            xembed.setDescription(message.translate("music/play:VOICE_CHANNEL_CONNECT"));
+            return message.channel.send(xembed);
+        }
+        voice.join()
+            .then(connection => {
+                connection.voice.setSelfDeaf(true)
+                xembed.setDescription(message.translate("music/play:JOIN"));
+                return message.channel.send(xembed);
+            });
+    }
 }
 module.exports = Join;
