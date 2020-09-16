81a7844462d6f5ce62fa06bc24e8b149a8bb7188
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-17 04:02:33 +0700

    Update filter.js

--- commands/Music/filter.js
+++ commands/Music/filter.js
@@ -34,7 +34,7 @@ class Filter extends Command {
         }
         const filters = [`3d`, `bassboost`, `echo`, `flanger`, `gate`, `haas`, `karaoke`, `nightcore`, `reverse`, `vaporwave`]
         if (!args[0]) {
-            xembed.setDescription(message.translate("music/filter:NO_ARGS") + "\n\n**" + filters.join("\n") + "**\u200b");
+            xembed.setDescription([`${message.translate("music/filter:NO_ARGS")}`, `\`\`\`${filters.join(", ")}\`\`\``]);
             return message.channel.send(xembed);
         }
         if (filters.includes(args[0])) {
