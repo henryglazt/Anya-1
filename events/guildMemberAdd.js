934d766f268dfb3192dc03ab032d8a5b81b346f1
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-17 23:46:46 +0700

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -60,8 +60,8 @@ module.exports = class {
 						ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
 						// Draw username
 						ctx.fillStyle = "#ffffff";
-						ctx.font = applyText(canvas, member.user.username, 68);
-						ctx.fillText(member.user.username, canvas.width - 210, canvas.height - 248);
+						ctx.font = applyText(canvas, member.user.username, 100);
+						ctx.fillText(member.user.username, canvas.width - 160, canvas.height - 248);
 						// Draw server name
 						/*ctx.font = applyText(canvas, member.guild.translate("administration/welcome:IMG_WELCOME", {
 							server: member.guild.name
@@ -71,7 +71,7 @@ module.exports = class {
 						}), canvas.width - 690, canvas.height - 65);*/
 						// Draw discriminator
 						ctx.font = "40px Bold";
-						ctx.fillText(member.user.discriminator, canvas.width - 173, canvas.height - 178);
+						ctx.fillText(member.user.discriminator, canvas.width - 123, canvas.height - 178);
 						// Draw number
 						ctx.font = "22px Bold";
 						ctx.fillText(member.guild.translate("administration/welcome:IMG_NB", {
@@ -80,17 +80,17 @@ module.exports = class {
 						// Draw # for discriminator
 						ctx.fillStyle = "#44d14a";
 						ctx.font = "75px SketchMatch";
-						ctx.fillText("#", canvas.width - 240, canvas.height - 165);
+						ctx.fillText("#", canvas.width - 190, canvas.height - 165);
 						// Draw Title with gradient
 						ctx.font = "90px Bold";
 						ctx.strokeStyle = "#1d2124";
 						ctx.lineWidth = 15;
-						ctx.strokeText(member.guild.translate("administration/welcome:TITLE"), canvas.width - 470, canvas.height - 330);
-						var gradient = ctx.createLinearGradient(canvas.width - 630, 0, canvas.width - 30, 0);
+						ctx.strokeText(member.guild.translate("administration/welcome:TITLE"), canvas.width - 420, canvas.height - 330);
+						var gradient = ctx.createLinearGradient(canvas.width - 580, 0, canvas.width - 30, 0);
 						gradient.addColorStop(0, "#e15500");
 						gradient.addColorStop(1, "#e7b121");
 						ctx.fillStyle = gradient;
-						ctx.fillText(member.guild.translate("administration/welcome:TITLE"), canvas.width - 470, canvas.height - 330);
+						ctx.fillText(member.guild.translate("administration/welcome:TITLE"), canvas.width - 420, canvas.height - 330);
                 
 						// Pick up the pen
 						ctx.beginPath();
