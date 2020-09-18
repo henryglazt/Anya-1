35c151fd0e5d56950b96d764cafc28a3d98a6fff
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-18 12:07:11 +0700

    Update invite.js

--- commands/General/invite.js
+++ commands/General/invite.js
@@ -17,32 +17,22 @@ class Invite extends Command {
 			cooldown: 5000
 		});
 	}
-
 	async run (message, args, data) {
-
 		const inviteLink = this.client.config.inviteURL || `https://discordapp.com/oauth2/authorize?client_id=${this.client.user.id}&scope=bot&permissions=8`;
 		const voteURL = this.client.config.voteURL || `https://discordbots.org/bot/${this.client.user.id}/vote`;
 		const supportURL = this.client.config.supportURL || await this.client.functions.supportLink(this.client);
-
 		if(args[0] && args[0] === "copy"){
 			return message.channel.send(inviteLink);
 		}
-        
 		const embed = new Discord.MessageEmbed()
 			.setAuthor(message.translate("general/invite:LINKS"))
-			.setDescription(message.translate("general/invite:TIP", {
-				prefix: data.guild.prefix
-			}))
+			.setDescription(message.translate("general/invite:TIP"))
 			.addField(message.translate("general/invite:ADD"), inviteLink)
 			.addField(message.translate("general/invite:VOTE"), voteURL)
 			.addField(message.translate("general/invite:SUPPORT"), supportURL)
 			.setColor(data.config.embed.color)
 			.setFooter(data.config.embed.footer);
-        
 		message.channel.send(embed);
-           
 	}
-
 }
-
 module.exports = Invite;
