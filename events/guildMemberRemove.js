78563cdcabee6c6080fbf00d32439c02d3bae7ac
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-20 03:00:06 +0700

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -39,51 +39,59 @@ module.exports = class {
 							ctx = canvas.getContext("2d");
                     
 						// Background language"
-						const background = await Canvas.loadImage("./assets/img/greetings_background.png");
+						const background = await Canvas.loadImage("./assets/img/greetings.jpeg");
 						// This uses the canvas dimensions to stretch the image onto the entire canvas
 						ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-						// Draw username
-						ctx.fillStyle = "#ffffff";
-						ctx.font = applyText(canvas, member.user.username, 48);
-						ctx.fillText(member.user.username, canvas.width - 660, canvas.height - 248);
-						// Draw server name
-						ctx.font = applyText(canvas, member.guild.translate("administration/goodbye:IMG_GOODBYE", {
 							server: member.guild.name
-						}), 53);
-						ctx.fillText(member.guild.translate("administration/goodbye:IMG_GOODBYE", {
-							server: member.guild.name
-						}), canvas.width - 690, canvas.height - 65);
-						// Draw discriminator
-						ctx.font = "40px Bold";
-						ctx.fillText(member.user.discriminator, canvas.width - 623, canvas.height - 178);
-						// Draw number
-						ctx.font = "22px Bold";
-						ctx.fillText(member.guild.translate("administration/goodbye:IMG_NB", {
-							memberCount: member.guild.memberCount
-						}), 40, canvas.height - 50);
+						}), canvas.width - 690, canvas.height - 65);*/
 						// Draw # for discriminator
 						ctx.fillStyle = "#44d14a";
+						ctx.shadowColor = "BLACK";
+						ctx.shadowBlur = 5;
 						ctx.font = "75px SketchMatch";
-						ctx.fillText("#", canvas.width - 690, canvas.height - 165);
+						ctx.fillText("#", canvas.width - 170, canvas.height - 138);
 						// Draw Title with gradient
 						ctx.font = "90px Bold";
 						ctx.strokeStyle = "#1d2124";
 						ctx.lineWidth = 15;
-						ctx.strokeText(member.guild.translate("administration/goodbye:TITLE"), canvas.width - 620, canvas.height - 330);
-						var gradient = ctx.createLinearGradient(canvas.width - 780, 0, canvas.width - 30, 0);
+						ctx.strokeText(member.guild.translate("administration/welcome:TITLE"), canvas.width - 430, canvas.height - 330);
+						var gradient = ctx.createLinearGradient(canvas.width - 590, 0, canvas.width - 30, 0);
 						gradient.addColorStop(0, "#e15500");
 						gradient.addColorStop(1, "#e7b121");
 						ctx.fillStyle = gradient;
-						ctx.fillText(member.guild.translate("administration/goodbye:TITLE"), canvas.width - 620, canvas.height - 330);
+						ctx.fillText(member.guild.translate("administration/welcome:TITLE"), canvas.width - 430, canvas.height - 330);
+						// Draw username
+						ctx.fillStyle = "WHITE";
+						ctx.shadowColor = "BLACK";
+						ctx.shadowBlur = 10;
+						ctx.textAlign = "right";
+						ctx.font = applyText(canvas, member.user.username, 70);
+						ctx.fillText(member.user.username, canvas.width - 20, canvas.height - 218);
+						// Draw discriminator
+						ctx.font = "40px Bold";
+						ctx.fillStyle = "WHITE";
+						ctx.shadowColor = "BLACK";
+						ctx.shadowBlur = 10;
+						ctx.fillText(member.user.discriminator, canvas.width - 20, canvas.height - 150);
+						// Draw number
+						ctx.font = "26px Bold";
+						ctx.fillStyle = "WHITE";
+						ctx.shadowColor = "BLACK";
+						ctx.shadowBlur = 5;
+						ctx.fillText(member.guild.translate("administration/welcome:IMG_NB", {
+							memberCount: member.guild.memberCount
+						}), 40, canvas.height - 30);
                 
 						// Pick up the pen
 						ctx.beginPath();
 						//Define Stroke Line
 						ctx.lineWidth = 10;
 						//Define Stroke Style
-						ctx.strokeStyle = "#df0909";
+						ctx.strokeStyle = "BLACK";
+						ctx.shadowColor = "BLACK";
+						ctx.shadowBlur = 10;
 						// Start the arc to form a circle
-						ctx.arc(180, 225, 135, 0, Math.PI * 2, true);
+						ctx.arc(512, 225, 135, 0, Math.PI * 2, true);
 						// Draw Stroke
 						ctx.stroke();
 						// Put the pen down
@@ -94,7 +102,7 @@ module.exports = class {
 						const options = { format: "png", size: 512 },
 							avatar = await Canvas.loadImage(member.user.displayAvatarURL(options));
 						// Move the image downwards vertically and constrain its height to 200, so it"s a square
-						ctx.drawImage(avatar, 45, 90, 270, 270);
+						ctx.drawImage(avatar, 377, 90, 270, 270);
 
 						const attachment = new Discord.MessageAttachment(canvas.toBuffer(), "goodbye-image.png");
 						channel.send(message, attachment);
@@ -107,4 +115,3 @@ module.exports = class {
 		});
 	}
 };
-  
