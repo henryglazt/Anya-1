d889dfaa1ad555c1b57a5e5be65c774ad9596a27
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-09-20 16:30:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,25 +3,25 @@ module.exports = {
 	token: "",
 	/* For the support server */
 	support: {
-		id: "339210008594087940", // The ID of the support server
-		logs: "736341595389165609", // And the ID of the logs channel of your server (new servers for example)
+		id: "718691607888789547", // The ID of the support server
+		logs: "732821616530030734", // And the ID of the logs channel of your server (new servers for example)
 	},
 	/* Dashboard configuration */
 	dashboard: {
 		enabled: false, // whether the dashboard is enabled or not
 		secret: "XXXXXXXXXXX", // Your discord client secret
-		baseURL: "https://dashboard.atlanta-bot.fr", // The base URl of the dashboard
+		baseURL: "", // The base URl of the dashboard
 		logs: "XXXXXXXXXXX", // The channel ID of logs
 		port: 8080, // Dashboard port
 		expressSessionPassword: "XXXXXXXXXXX", // Express session password (it can be what you want)
-		failureURL: "https://www.atlanta-bot.fr" // url on which users will be redirected if they click the cancel button (discord authentication)
+		failureURL: "" // url on which users will be redirected if they click the cancel button (discord authentication)
 	},
-	mongoDB: "mongodb+srv://nuruaruvi:laililham@nuruaruvi.nw8ve.azure.mongodb.net/nuruaruvi?retryWrites=true&w=majority", // The URl of the mongodb database
-	prefix: "?!", // The default prefix for the bot
+	mongoDB: "mongodb+srv://anyageraldine:laililham@cluster0.efdfb.mongodb.net/anya?retryWrites=true&w=majority", // The URl of the mongodb database
+	prefix: "??", // The default prefix for the bot
 	/* For the embeds (embeded messages) */
 	embed: {
-		color: "#8300ff", // The default color for the embeds
-		footer: "NuruAruvi" // And the default footer for the embeds
+		color: "#f1c40f", // The default color for the embeds
+		footer: "Anya Geraldine ~ Gang Sebelah © 2020" // And the default footer for the embeds
 	},
 	/* Bot's owner informations */
 	owner: {
@@ -49,11 +49,11 @@ module.exports = {
 	/* The Bot status */
 	status: [
 		{
-			name: "My own voice",
+			name: "@mention to ??help",
 			type: "LISTENING"
 		},
 		{
-			name: "And making love",
+			name: "at discord.gg/gangsebelah",
 			type: "PLAYING"
 		}
 	],
