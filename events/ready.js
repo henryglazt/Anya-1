7c0b46cad9216ef4cdef6a03d3f2b6256ee87311
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-10-13 18:19:11 +0700

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,46 @@
+const { timezone, rules } = require("../schedule");
+const { CronJob } = require("cron");
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+};
+
 module.exports = class {
 
-    constructor(client) {
+    constructor(client, rule) {
         this.client = client;
+        this.rule = rule;
     }
+
+  getChannelIds() {
+    switch (this.rule.sendPolicy) {
+      case "random": {
+        const randomIndex = getRandomInt(0, this.rule.channelIds.length);
+        return [this.rule.channelIds[randomIndex]];
+      }
+      case "all":
+      default:
+        return this.rule.channelIds;
+    }
+  }
+
+  async getWebhook(channelId) {
+    const channel = await this.client.channels.fetch(channelId);
+    const webhooks = await channel.fetchWebhooks();
+
+    return !webhooks.size
+      ? channel.createWebhook(this.client.user.username)
+      : webhooks.first();
+  }
+
+  sendMessage() {
+    this.getChannelIds().forEach(async (channelId) => {
+      const webhook = await this.getWebhook(channelId);
+      webhook.send(this.rule.content, { embeds: this.rule.embeds });
+    });
+  }
 
     async run() {
 
@@ -53,6 +91,17 @@ module.exports = class {
             if (status[parseInt(i + 1, 10)]) i++;
             else i = 0;
         }, 20000); // Every 20 seconds
+
+  rules.forEach((rule) => {
+    const bot = new CronBot(client, rule);
+    new CronJob(
+      rule.cronExpression,
+      () => bot.sendMessage(),
+      null,
+      true,
+      timezone
+    );
+  });
 
     }
 };
