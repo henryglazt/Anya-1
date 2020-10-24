ae1d327e3685e61bc4b18412a7fdd0b9b38ccdf4
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-10-24 22:07:30 +0700

    Create settempvoice.js

---
+++ commands/Administration/settempvoice.js
@@ -0,0 +1,39 @@
+const Command = require("../../base/Command.js");
+
+class Settempvoice extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "settempvoice",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: ["settmpvc"],
+            memberPermissions: ["MANAGE_GUILD"],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 3000
+        });
+    }
+
+    async run(message, args, data) {
+
+        const options = {
+            childCategory: message.member.voice.channel.parentID,
+            childAutoDelete: true,
+            childAutoDeleteIfOwnerLeaves: false,
+            childBitrate: 8000,
+            childFormat: (member) => `${member.user.username}`
+        };
+        this.client.tempChannels.registerChannel(message.member.voice.channel.id, options);
+        data.guild.tmpch = {
+            channelID: message.member.voice.channel.id,
+            options: options
+        };
+        data.guild.save();
+        message.success("administration/settmpvc:SUCCESS");
+    }
+}
+
+module.exports = Settempvoice;
