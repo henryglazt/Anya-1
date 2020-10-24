c52d5c24a9752a540c7d49a393e69c68c44526df
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-10-24 22:39:45 +0700

    Update vclimit.js

--- commands/Voice Channel/vclimit.js
+++ commands/Voice Channel/vclimit.js
@@ -1,1 +1,50 @@
-lla
+const Command = require("../../base/Command.js");
+
+class Vclimit extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "vclimit",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: [],
+            memberPermissions: [],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 3000
+        });
+    }
+
+    async run(message, args, data) {
+
+        if (message.deletable) message.delete();
+
+        const limit = args[0];
+
+        if (!message.member.voice.channel) return message.reply("You're not in voice channel.")
+            .then(m => {
+                m.delete({
+                    timeout: 5000
+                })
+            });
+        if (isNaN(args[0]) || !args[0]) return message.reply("Please provide a valid number to limit your voice channel")
+            .then(m => {
+                m.delete({
+                    timeout: 5000
+                })
+            });
+        message.member.voice.channel.setUserLimit(limit);
+        message.reply("Done!")
+            .then(m => {
+                m.delete({
+                    timeout: 5000
+                })
+            });
+    }
+}
+
+}
+
+module.exports = Vclimit;
