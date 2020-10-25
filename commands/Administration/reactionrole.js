ce62aac540d7108fe1b104908c49767368ffa822
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-10-25 22:41:30 +0700

    Update reactionrole.js

--- commands/Administration/reactionrole.js
+++ commands/Administration/reactionrole.js
@@ -1,1 +1,19 @@
-tes
+        const role = message.mentions.roles.first();
+        if (!role)
+            return message.reply('You need mention a role').then(m => m.delete({ timeout: 1_000 }));
+
+        const emoji = client.emojis.resolveIdentifier(args[1])
+        if (!emoji)
+            return message.reply('You need use a valid emoji.').then(m => m.delete({ timeout: 1_000 }));
+
+        const msg = await message.channel.messages.fetch(args[2] || message.id);
+        if (!role)
+            return message.reply('Message not found! Wtf...').then(m => m.delete({ timeout: 1_000 }));
+
+        reactionRoleManager.addRole({
+            message: msg,
+            role,
+            emoji
+        });
+        message.reply('Done').then(m => m.delete({ timeout: 500 }));
+        message.delete();
