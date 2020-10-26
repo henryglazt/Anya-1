3406654d65204fa515fd321deb3d3ce5de0ae015
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-10-27 01:39:51 +0700

    Create vclimit.js

---
+++ commands/Moderation/vclimit.js
@@ -0,0 +1,48 @@
+const Command = require("../../base/Command.js");
+
+class Vclimit extends Command {
+
+    constructor(client) {
+        super(client, {
+            name: "vclimit",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: [],
+            memberPermissions: ["MANAGE_MESSAGES"],
+            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 3000
+        });
+    }
+
+    async run(message, args, data) {
+
+        if (message.deletable) message.delete();
+
+        const limit = args[0];
+
+        if (!message.member.voice.channel) return message.error("voice/main:ERROR")
+            .then(m => {
+                m.delete({
+                    timeout: 5000
+                })
+            });
+        if (isNaN(args[0]) || !args[0] || args[0] > 99) return message.error("voice/main:ENUM")
+            .then(m => {
+                m.delete({
+                    timeout: 5000
+                })
+            });
+        message.member.voice.channel.setUserLimit(limit);
+        message.success("voice/main:SUCCESS")
+            .then(m => {
+                m.delete({
+                    timeout: 5000
+                })
+            });
+    }
+}
+
+module.exports = Vclimit;
