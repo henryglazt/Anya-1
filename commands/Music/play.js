faccd94e760a109d2dba73779e7def747b1177c7
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-01 22:13:13 +0700

    Update play.js

--- commands/Music/play.js
+++ commands/Music/play.js
@@ -33,53 +33,80 @@ class Play extends Command {
             embed.setDescription(message.translate("music/play:VOICE_CHANNEL_CONNECT"));
             return message.channel.send(embed);
         }
-        const string = args.join(" ");
-        if (!string) {
+        const search = args.join(" ");
+        if (!search) {
             embed.setDescription(message.translate("music/play:MISSING_SONG_NAME"));
             return message.channel.send(embed);
         }
-        /*try {
-            voice.join()
-                .then(connection => {
-                    connection.voice.setSelfDeaf(true)
-                });
-            this.client.distube.play(message, string);
-        } catch (e) {
-            message.error(`Error: \`${e}\``)
-        }*/
-      const client = this.client;
-      
-      const res = await client.manager.search(
-      args,
-      message.author
-    );
 
-    // Create a new player. This will return the player if it already exists.
-    const player = client.manager.create({
+    const player = message.client.manager.create({
       guild: message.guild.id,
-      voiceChannel: message.member.voice.channel.id,
+      voiceChannel: channel.id,
       textChannel: message.channel.id,
     });
 
-    // Connect to the voice channel.
     player.connect();
 
-    // Adds the first track to the queue.
-    player.queue.add(res.tracks[0]);
-    message.channel.send(`Enqueuing track ${res.tracks[0].title}.`);
+    let res;
 
-    // Plays the player (plays the first track in the queue).
-    // The if statement is needed else it will play the current track again
-    if (!player.playing && !player.paused && !player.queue.length)
-      player.play();
+    try {
+      res = await player.search(search, message.author);
+      if (res.loadType === 'LOAD_FAILED') {
+        if (!player.queue.current) player.destroy();
+        throw res.exception;
+      }
+    } catch (err) {
+      return message.reply(`there was an error while searching: ${err.message}`);
+    }
 
-    // For playlists you'll have to use slightly different if statement
-    if (
-      !player.playing &&
-      !player.paused &&
-      player.queue.size === res.tracks.length
-    )
-      player.play();
+    switch (res.loadType) {
+      case 'NO_MATCHES':
+        if (!player.queue.current) player.destroy();
+        return message.reply('there were no results found.');
+      case 'TRACK_LOADED':
+        player.queue.add(res.tracks[0]);
+
+        if (!player.playing && !player.paused && !player.queue.length) player.play();
+        return message.reply(`enqueuing \`${res.tracks[0].title}\`.`);
+      case 'PLAYLIST_LOADED':
+        player.queue.add(res.tracks);
+
+        if (!player.playing && !player.paused && player.queue.size === res.tracks.length) player.play();
+        return message.reply(`enqueuing playlist \`${res.playlist.name}\` with ${res.tracks.length} tracks.`);
+      case 'SEARCH_RESULT':
+        let max = 5, collected, filter = (m) => m.author.id === message.author.id && /^(\d+|end)$/i.test(m.content);
+        if (res.tracks.length < max) max = res.tracks.length;
+
+        const results = res.tracks
+            .slice(0, max)
+            .map((track, index) => `${++index} - \`${track.title}\``)
+            .join('\n');
+
+        message.channel.send(results);
+
+        try {
+          collected = await message.channel.awaitMessages(filter, { max: 1, time: 30e3, errors: ['time'] });
+        } catch (e) {
+          if (!player.queue.current) player.destroy();
+          return message.reply("you didn't provide a selection.");
+        }
+
+        const first = collected.first().content;
+
+        if (first.toLowerCase() === 'end') {
+          if (!player.queue.current) player.destroy();
+          return message.channel.send('Cancelled selection.');
+        }
+
+        const index = Number(first) - 1;
+        if (index < 0 || index > max - 1) return message.reply(`the number you provided too small or too big (1-${max}).`);
+
+        const track = res.tracks[index];
+        player.queue.add(track);
+
+        if (!player.playing && !player.paused && !player.queue.length) player.play();
+        return message.reply(`enqueuing \`${track.title}\`.`);
+    }
       
     }
 }
