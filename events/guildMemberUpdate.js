2f9070c3e0bcdaafc4071acc3e1af436e6d3c836
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-05 18:18:57 +0700

    Update guildMemberUpdate.js

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -17,18 +17,18 @@ module.exports = class {
 			newMember.send({ files: [ { name: "unlocked.png", attachment: "./assets/img/achievements/achievement_unlocked5.png"} ] });
 		}
 		if (!oldMember.premiumSince && newMember.premiumSince) {
-		const channel = newMember.guild.channels.cache.get("722420259721838653");
+		const channel = newMember.guild.channels.cache.get("773711236117823509");
 		const bembed = new Discord.MessageEmbed()
 			.setColor("#f47fff")
 			.setAuthor(`${newMember.displayName} just boosted the server!`, "https://cdn.discordapp.com/emojis/739595657123201125.gif")
 			.setThumbnail(`${newMember.user.displayAvatarURL({ dynamic: true })}`)
-			.setDescription(`Thank you ${newMember.user} for boosting the server.\nBecause of you, we are now have __**${newMember.guild.premiumSubscriptionCount}**__ boost in total.\nPlease DM our Admin or Moderator for a __**Custom Role!**__`)
+			.setDescription(`Thank you ${newMember.user} for boosting the server.\nBecause of you, we are now have __**${newMember.guild.premiumSubscriptionCount}**__ boost in total.`)
 			.setFooter(`Current Server Level: ${newMember.guild.premiumTier}`, "https://cdn.discordapp.com/emojis/739595657123201125.gif")
 			.setTimestamp();
 		channel.send(bembed).catch(console.error);
 		}
 		if (oldMember.premiumSince && !newMember.premiumSince) {
-			newMember.roles.add("733419579585331320");
+			newMember.roles.add("773868547444441111");
 		}
 	}
 };
