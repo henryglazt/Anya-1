95078d0e0ad8bdcc0bc7d86f090d024e794be36b
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-08 12:30:29 +0700

    Update queue.js

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -9,9 +9,9 @@ class Queue extends Command {
             dirname: __dirname,
             enabled: true,
             guildOnly: true,
-            aliases: ["q"],
+            aliases: [ "q" ],
             memberPermissions: [],
-            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
             nsfw: false,
             ownerOnly: false,
             cooldown: 5000
@@ -24,7 +24,7 @@ class Queue extends Command {
             .setColor(data.config.embed.color)
             .setFooter(data.config.embed.footer)
 
-        const queue = this.client.distube.getQueue(message.guild.id);
+        /*const queue = this.client.distube.getQueue(message.guild.id);
 
         if (!queue) {
             xembed.setDescription(message.translate("music/play:NOT_PLAYING"));
@@ -125,7 +125,33 @@ class Queue extends Command {
             // Remove the reaction when the user react to the message
             await reaction.users.remove(message.author.id);
 
-        });
+        });*/
+
+    const player = message.client.manager.players.get(message.guild.id);
+    if (!player) return message.reply("I have not joined a channel because I have nothing to play. Use the play command to play the song.");
+
+    const queue = player.queue;
+    const embed = new MessageEmbed().setAuthor(`Queue for ${message.guild.name}`);
+
+    const multiple = 10;
+    const page = args.length && Number(args[0]) ? Number(args[0]) : 1;
+
+    const end = page * multiple;
+    const start = end - multiple;
+
+    const tracks = queue.slice(start, end);
+
+    if (queue.current) embed.addField("Current", `[${queue.current.title}](${queue.current.uri})`);
+
+    if (!tracks.length) embed.setDescription(`No tracks in ${page > 1 ? `page ${page}` : "the queue"}.`);
+    else embed.setDescription(tracks.map((track, i) => `${start + (++i)} - [${track.title}](${track.uri})`).join("\n"));
+
+    const maxPages = Math.ceil(queue.length / multiple);
+
+    embed.setFooter(`Page ${page > maxPages ? maxPages : page} of ${maxPages}`);
+
+    return message.reply(embed);
+
     }
 
 }
