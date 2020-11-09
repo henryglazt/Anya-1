ae1813ad26b2ec300f80dd87a4e84c655a00320c
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-09 19:37:06 +0700

    Update queue.js

--- commands/Music/queue.js
+++ commands/Music/queue.js
@@ -169,7 +169,7 @@ class Queue extends Command {
     if(queue.current) embed.addField("Now Playing", `[${queue.current.title}](${queue.current.uri}) | \`${player.queue.current.requester.tag}\``);
 
     if(!tracks.length) embed.setDescription(`No tracks in ${page > 1 ? `Page ${page}` : "the queue"}.`);
-    else embed.setDescription(tracks.map((track, i) => `**${start + (++i)} -** [${track.title}](${track.uri})`).join("\n"));
+    else embed.setDescription(tracks.map((track, i) => `**${start + (++i)} -** ${track.title}${track.uri}`).join("\n"));
 
     const maxPages = Math.ceil(queue.length / multiple);
     embed.setColor(data.config.embed.color)
