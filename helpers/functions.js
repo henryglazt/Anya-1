430055e0f7ff34c19dac33cb8dcc01420d1ecd1f
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-16 00:48:12 +0700

    Update functions.js

--- helpers/functions.js
+++ helpers/functions.js
@@ -153,7 +153,7 @@ module.exports = {
                     if (v === 0 && !first) {
                         continue;
                     }
-                    finalTime.push(v < 10 ? `0${v}` : `${v}`);
+                    finalTime.push(v < 10 ? `0${v}` : `0${v}`);
                     first = true;
                     continue;
                 }
