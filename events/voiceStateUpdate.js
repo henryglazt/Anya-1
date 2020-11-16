2ac3bbe7dbc4e0ac06a963f285b523bcb74cb72b
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-16 12:49:55 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,25 +1,30 @@
+const { MessageEmbed } = require("discord.js");
 module.exports = class {
-	constructor (client) {
-		this.client = client;
-	}
-
-	async run (oldState, newState) {
-    
-    const client = this.client;
-    
-    const player = client.manager.players.get(newState.guild.id);
-
-    if(!player) return;
-
-    const channel = client.channels.cache.get(player.textChannel);
-    
-    let chnl = player.voiceChannel;
-
-    let guild = player.guild;
-
-    if(client.guilds.cache.get(guild).channels.cache.get(chnl).members.filter(m => !m.user.bot).size < 1) {
-        channel.send(newState.guild.translate("music/stop:LEAVE"));
-        return player.destroy()
+    constructor(client) {
+        this.client = client;
     }
-	}
+
+    async run(oldState, newState) {
+
+        const client = this.client;
+
+        const player = client.manager.players.get(newState.guild.id);
+        if (!player) return;
+
+        const channel = client.channels.cache.get(player.textChannel);
+
+        let chnl = player.voiceChannel;
+        let guild = player.guild;
+
+        let embed = new MessagEmbed()
+            .setColor(client.config.embed.color)
+            .setFooter(client.config.embed.footer)
+            .setImage("https://cdn.discordapp.com/emojis/754574913209368687.gif")
+            .setAuthor(newState.guild.translate("music/stop:LEAVE"));
+
+        if (client.guilds.cache.get(guild).channels.cache.get(chnl).members.filter(m => !m.user.bot).size < 1) {
+            channel.send(embed);
+            return player.destroy()
+        }
+    }
 };
