11750b0adc10d8cf77f2a676a59b2673eb16df8e
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-17 07:04:52 +0700

    Update functions.js

--- helpers/functions.js
+++ helpers/functions.js
@@ -199,6 +199,16 @@ module.exports = {
                 return null;
             }
             return duration;
-        }
+        },
+        formatDuration(durationObj) {
+            const duration = `${durationObj.hours ? (durationObj.hours + ':') : ''}${durationObj.minutes ? durationObj.minutes : '00'
+                }:${(durationObj.seconds < 10)
+                    ? ('0' + durationObj.seconds)
+                    : (durationObj.seconds
+                        ? durationObj.seconds
+                        : '00')
+                }`;
+            return duration;
+    };
 
 };
