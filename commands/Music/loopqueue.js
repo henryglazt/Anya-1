773b62d2be289efc506e89b91506728f5f6d24dd
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-20 17:00:44 +0700

    Create loopqueue.js

---
+++ commands/Music/loopqueue.js
@@ -0,0 +1,50 @@
+const Command = require("../../base/Command.js"),
+    { MessageEmbed } = require("discord.js");
+class Loopqueue extends Command {
+    constructor(client) {
+        super(client, {
+            name: "loopqueue",
+            dirname: __dirname,
+            enabled: true,
+            guildOnly: true,
+            aliases: [ "lq" ],
+            memberPermissions: [],
+            botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
+            nsfw: false,
+            ownerOnly: false,
+            cooldown: 5000
+        });
+    }
+    async run(message, args, data) {
+
+        const musji = this.client.customEmojis.music;
+        const embed = new MessageEmbed()
+            .setColor(data.config.embed.color)
+            .setFooter(data.config.embed.footer)
+
+        const player = message.client.manager.players.get(message.guild.id);
+        const { channel } = message.member.voice;
+        if (!channel) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:NO_VOICE_CHANNEL"));
+            return message.channel.send(embed);
+        }
+        if (!player) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:NOT_PLAYING"));
+            return message.channel.send(embed);
+        }
+        if (channel.id !== player.voiceChannel) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:MY_VOICE_CHANNEL"));
+            return message.channel.send(embed);
+        }
+        if (!player.trackRepeat) {
+            player.setQueueRepeat(true);
+            embed.setDescription(musji.repeatall + " " + message.translate("music/lq:SUCCESS"));
+            return message.channel.send(embed);
+        } else {
+            player.setQueueRepeat(false);
+            embed.setDescription(musji.repeatoff + " " + message.translate("music/lq:OFF"));
+            return message.channel.send(embed);
+        };
+    }
+}
+module.exports = Loopqueue;
