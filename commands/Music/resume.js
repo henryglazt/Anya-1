cd9c899b776871be3984659b192a71c53340b0cb
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-20 13:13:37 +0700

    Update resume.js

--- commands/Music/resume.js
+++ commands/Music/resume.js
@@ -1,5 +1,5 @@
 const Command = require("../../base/Command.js"),
-    Discord = require("discord.js");
+    { MessageEmbed } = require("discord.js");
 class Resume extends Command {
 
     constructor(client) {
@@ -10,36 +10,40 @@ class Resume extends Command {
             guildOnly: true,
             aliases: [],
             memberPermissions: [],
-            botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+            botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
             nsfw: false,
             ownerOnly: false,
             cooldown: 5000
         });
     }
     async run(message, args, data) {
-        const xembed = new Discord.MessageEmbed()
+
+        const musji = this.client.customEmojis.music;
+        const embed = new MessageEmbed()
             .setColor(data.config.embed.color)
             .setFooter(data.config.embed.footer)
-        const voice = message.member.voice.channel;
-        if (!voice) {
-            xembed.setDescription(message.translate("music/play:NO_VOICE_CHANNEL"));
-            return message.channel.send(xembed);
+
+        const player = message.client.manager.players.get(message.guild.id);
+        const { channel } = message.member.voice;
+        if (!channel) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:NO_VOICE_CHANNEL"));
+            return message.channel.send(embed);
         }
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-            xembed.setDescription(message.translate("music/play:MY_VOICE_CHANNEL"));
-            return message.channel.send(xembed);
+        if (!player) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:NOT_PLAYING"));
+            return message.channel.send(embed);
         }
-        if (!this.client.distube.isPlaying(message)) {
-            xembed.setDescription(message.translate("music/play:NOT_PLAYING"));
-            return message.channel.send(xembed);
+        if (channel.id !== player.voiceChannel) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:MY_VOICE_CHANNEL"));
+            return message.channel.send(embed);
         }
-        if (!queue.dispatcher.paused) {
-            xembed.setDescription(message.translate("music/resume:NOT_PAUSED"));
-            return message.channel.send(xembed);
+        if (!player.paused) {
+            embed.setDescription(musji.info + " " + message.translate("music/resume:NOT_PAUSED"));
+            return message.channel.send(embed);
         } else {
-            queue.dispatcher.resume();
-            xembed.setDescription(message.translate("music/resume:SUCCESS"));
-            return message.channel.send(xembed);
+            player.paused(false);
+            embed.setDescription(musji.play + " " + message.translate("music/resume:SUCCESS"));
+            return message.channel.send(embed);
         }
     }
 }
