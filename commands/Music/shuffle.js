e6c0ce6ea95566e4a4ae8ba998220505c0ee3a25
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-22 20:23:45 +0700

    Update shuffle.js

--- commands/Music/shuffle.js
+++ commands/Music/shuffle.js
@@ -7,9 +7,9 @@ class Shuffle extends Command {
 			dirname: __dirname,
 			enabled: true,
 			guildOnly: true,
-			aliases: ["acak"],
+			aliases: [ "acak" ],
 			memberPermissions: [],
-			botPermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+			botPermissions: [ "SEND_MESSAGES", "EMBED_LINKS" ],
 			nsfw: false,
 			ownerOnly: false,
 			cooldown: 5000
@@ -23,17 +23,22 @@ class Shuffle extends Command {
             .setFooter(data.config.embed.footer)
 
         const player = message.client.manager.players.get(message.guild.id);
-        const { channel } = message.member.voice;
+        let sid = player.get("voiceData").session;
+        const { channel, sessionID } = message.member.voice;
         if (!channel) {
             embed.setDescription(musji.info + " " + message.translate("music/play:NO_VOICE_CHANNEL"));
             return message.channel.send(embed);
         }
-        if (!player || !player.playing || !player.playing && !player.paused || player.queue.totalSize === 0) {
+        if (!player || !player.queue.current) {
             embed.setDescription(musji.info + " " + message.translate("music/play:NOT_PLAYING"));
             return message.channel.send(embed);
         }
         if (channel.id !== player.voiceChannel) {
             embed.setDescription(musji.info + " " + message.translate("music/play:MY_VOICE_CHANNEL"));
+            return message.channel.send(embed);
+        }
+        if (sessionID !== sid) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:SESSION"));
             return message.channel.send(embed);
         }
         if (player.queue.size <= 2) {
