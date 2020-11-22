374a5c93b2b7361320ec9277a816aa60a6423b9c
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-22 20:17:22 +0700

    Update stop.js

--- commands/Music/stop.js
+++ commands/Music/stop.js
@@ -23,17 +23,22 @@ class Stop extends Command {
             .setFooter(data.config.embed.footer)
 
         const player = message.client.manager.players.get(message.guild.id);
-        const { channel } = message.member.voice;
+        let sid = player.get("voiceData").session;
+        const { channel, sessionID } = message.member.voice;
         if (!channel) {
-            embed.setDescription(message.translate("music/play:NO_VOICE_CHANNEL"));
+            embed.setDescription(musji.info + " " + message.translate("music/play:NO_VOICE_CHANNEL"));
             return message.channel.send(embed);
         }
         if (!player) {
-            embed.setDescription(message.translate("music/play:NOT_PLAYING"));
+            embed.setDescription(musji.info + " " + message.translate("music/play:NOT_PLAYING"));
             return message.channel.send(embed);
         }
         if (channel.id !== player.voiceChannel) {
-            embed.setDescription(message.translate("music/play:MY_VOICE_CHANNEL"));
+            embed.setDescription(musji.info + " " + message.translate("music/play:MY_VOICE_CHANNEL"));
+            return message.channel.send(embed);
+        }
+        if (sessionID !== sid) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:SESSION"));
             return message.channel.send(embed);
         }
         if (!player.queue.current) {
@@ -50,68 +55,6 @@ class Stop extends Command {
         }));
         embed.setImage("https://cdn.discordapp.com/attachments/773766203914321980/773785370503806976/banner_serverr_10.png");
         return message.channel.send(embed);
-
-        /*const members = voice.members.filter((m) => !m.user.bot);
-        const embed = new Discord.MessageEmbed()
-            .setAuthor(message.translate("music/stop:DESCRIPTION"))
-            .setFooter(data.config.embed.footer)
-            .setColor(data.config.embed.color);
-        const m = await message.channel.send(embed);
-        if (members.size > 1) {
-            m.react("⏹️");
-            const mustVote = Math.floor(members.size / 2 + 1);
-            embed.setDescription(message.translate("music/stop:VOTE_CONTENT", {
-                voteCount: 0,
-                requiredCount: mustVote
-            }));
-            m.edit(embed);
-            const filter = (reaction, user) => {
-                const member = message.guild.members.cache.get(user.id);
-                const voiceChannel = member.voice.channel;
-                if (voiceChannel) {
-                    return voiceChannel.id === voice.id;
-                }
-            };
-            const collector = await m.createReactionCollector(filter, {
-                time: 25000
-            });
-            collector.on("collect", (reaction) => {
-                const haveVoted = reaction.count - 1;
-                if (haveVoted >= mustVote) {
-                    this.client.distube.stop(message);
-                    message.channel.send({
-                        embed: {
-                            color: data.config.embed.color,
-                            footer: {
-                                text: data.config.embed.footer
-                            },
-                            description: message.translate("music/stop:SUCCESS")
-                        }
-                    });
-                    collector.stop(true);
-                } else {
-                    embed.setDescription(message.translate("music/stop:VOTE_CONTENT", {
-                        voteCount: haveVoted,
-                        requiredCount: mustVote
-                    }));
-                    m.edit(embed);
-                }
-            });
-            collector.on("end", (collected) => {
-                return m.reactions.removeAll();
-            });
-        } else {
-            this.client.distube.stop(message);
-            m.edit({
-                embed: {
-                    color: data.config.embed.color,
-                    footer: {
-                        text: data.config.embed.footer
-                    },
-                    description: message.translate("music/stop:SUCCESS")
-                }
-            });
-        }*/
     }
 }
 module.exports = Stop;
