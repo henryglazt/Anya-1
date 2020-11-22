7626962874b38419284121c3de0c8410ae5fa210
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-11-23 05:06:43 +0700

    Update move.js

--- commands/Music/move.js
+++ commands/Music/move.js
@@ -24,7 +24,7 @@ class Move extends Command {
             .setFooter(data.config.embed.footer)
 
         const player = message.client.manager.players.get(message.guild.id);
-        let sid = player.get("voiceData").session;
+        let v = player.get("voiceData");
         const { channel, sessionID } = message.member.voice;
         if (!channel) {
             embed.setDescription(musji.info + " " + message.translate("music/play:NO_VOICE_CHANNEL"));
@@ -38,8 +38,8 @@ class Move extends Command {
             embed.setDescription(musji.info + " " + message.translate("music/play:MY_VOICE_CHANNEL"));
             return message.channel.send(embed);
         }
-        if (sessionID !== sid) {
-            embed.setDescription(musji.info + " " + message.translate("music/play:SESSION"));
+        if (sessionID !== v.session) {
+            embed.setDescription(musji.info + " " + message.translate("music/play:SESSION", {user: "<@" + v.id + ">"}));
             return message.channel.send(embed);
         }
         if (!player.queue.size) {
@@ -63,45 +63,6 @@ class Move extends Command {
            indexTo: to
         }));
         return message.channel.send(embed);
-
-        /*const embed = new Discord.MessageEmbed()
-            .setColor(data.config.embed.color)
-            .setFooter(data.config.embed.footer)
-        const number = args[0];
-        const queue = this.client.distube.getQueue(message);
-        const voice = message.member.voice.channel;
-        if (!voice) {
-            embed.setDescription(message.translate("music/play:NO_VOICE_CHANNEL"));
-            return message.channel.send(embed);
-        }
-        if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) {
-            embed.setDescription(message.translate("music/play:MY_VOICE_CHANNEL"));
-            return message.channel.send(embed);
-        }
-        if (!this.client.distube.isPlaying(message)) {
-            embed.setDescription(message.translate("music/play:NOT_PLAYING"));
-            return message.channel.send(embed);
-        }
-        if (!queue.songs[1]) {
-            embed.setDescription(message.translate("music/skip:NO_NEXT_SONG"));
-            return message.channel.send(embed);
-        }
-        if (!number) {
-            embed.setDescription(message.error("music/remove:VALUE"));
-            return message.channel.send(embed);
-        }
-        if (number <= 0) {
-            embed.setDescription(message.error("music/remove:VALUE"));
-            return message.channel.send(embed);
-        }
-        if (isNaN(number)) {
-            embed.setDescription(message.error("music/remove:VALUE"));
-            return message.channel.send(embed);
-        }
-        const song = queue.songs[number];
-        embed.setDescription(message.translate("music/remove:SUCCESS", {songName: song.name, songURL: song.url}));
-        message.channel.send(embed);
-        return queue.songs.splice(number, 1);*/
     }
 }
 module.exports = Move;
