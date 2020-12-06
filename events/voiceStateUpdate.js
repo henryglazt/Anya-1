849f1750e641e6254b22ab49e68c55738efa1863
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-07 03:02:35 +0700

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -59,11 +59,13 @@ module.exports = class {
 
 
         const voiceCh = client.channels.cache.get("773742429505454081");
-        if (newState.channelID === voiceCh.id) {
-            const target = newState.member;
-            const ch = await voiceCh.clone({ name: target.user.username });
-            if (!ch.deleted && target.user.username.toLowerCase() === ch.name.toLowerCase()) {
-                return target.setChannel(ch);
+        if (oldState !== newState) {
+            if (newState.channelID === voiceCh.id) {
+                const target = newState.member;
+                const ch = await voiceCh.clone({ name: target.user.username });
+                if (!ch.deleted && target.user.username.toLowerCase() === ch.name.toLowerCase()) {
+                    return target.setChannel(ch);
+                }
             }
         }
         const vcUser = ch.members.filter(m => !m.user.bot).size < 1;
