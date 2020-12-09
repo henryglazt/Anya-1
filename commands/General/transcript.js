1daffc65059edff203bc423847de6070061a911e
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-09 11:02:57 +0700

    Update transcript.js

--- commands/General/transcript.js
+++ commands/General/transcript.js
@@ -1,7 +1,7 @@
 const Command = require("../../base/Command.js"),
 	fs = require("fs").promises,
 		moment = require("moment"),
-			{ Collection, MessageAttachment } = require("discord.js");
+			{ MessageAttachment } = require("discord.js");
 
 class Transcript extends Command {
 
@@ -24,22 +24,26 @@ class Transcript extends Command {
 
                 await message.delete();
 
-                let text = [];
-                let msg = await message.channel.messages.fetch({ limit: 100 });
+		let att = [];
+		let text = [];
+		let msg = await message.channel.messages.fetch({ limit: 100 });
 
 		moment.locale("id");
 
                 msg.forEach(m => {
-                      	text.push(`${m.author.tag}:\n${m.content}\n${moment(m.createdTimestamp).format("L")} - ${moment(m.createdTimestamp).format("LT")}\n\n`);
+			let x = m.attachments.map(a => a.proxyURL);
+			if (x.length > 0) att.push(x);
+			text.push(`${m.author.tag}:\nMessage: ${m.content}\nAttacments:n${moment(m.createdTimestamp).format("L")} - ${moment(m.createdTimestamp).format("LT")}\n\n`);
                 });
 
-		text = text.reverse().join(" ");
+		att = att.join("\n");
+		text = text.reverse().join("");
 
 		let data = await fs.readFile("./transcript.txt", "utf8").catch(err => console.error(err));
 		if (data) {
 			await fs.writeFile("index.txt", text).catch(err => console.error(err));
 			let attachment = new MessageAttachment("./index.txt", `Ticket ${message.author.tag}.txt`);
-			return message.channel.send(attachment);
+			return message.channel.send([attachment, att]);
 		}
 
 	}
