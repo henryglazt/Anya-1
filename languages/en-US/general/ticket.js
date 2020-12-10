7161f7f554734d1029eab83114bb187999dc0612
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-11 02:37:57 +0700

    Create ticket.js

---
+++ languages/en-US/general/ticket.js
@@ -0,0 +1,15 @@
+{
+    "DESCRIPTION": "Opens or close a ticket",
+    "USAGE": "{{prefix}}ticket [open/close] [reason]",
+    "EXAMPLES": "{{prefix}}ticket open report",
+    "DISABLED": "Pong! My ping is `{{ping}}` ms.",
+    "MISSING_ROLE": "Theres some missing role, please contact the staffs here!",
+    "MISSING_CHANNEL": "Theres some missing channel, please contact the staffs here!",
+    "NO_REASON": "You must specify a reason why you opening a ticket!",
+    "LIMIT_CHAR": "Reason must not exceed 20 characters!",
+    "WRONG_CHANNEL": "Go to {{channel}} to open a new ticket!",
+    "RESOLVE_FALSE": "You already have an open ticket here {{channel}}",
+    "OPEN": "Your new ticket is opened here {{channel}}",
+    "CLOSE": "This is your last ticket transcript.",
+    "EMBED_DESC": "Welcome {{user}}\n\nPlease describe the reasoning for opening this ticket, include any information you think may be relevant such as proof, other third parties and so on.\n\nUse the following command to close the ticket\n`-ticket close [reason]`"
+}
