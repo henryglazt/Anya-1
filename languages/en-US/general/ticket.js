a253553941514a0fd4dddd26e261735331cbe1fa
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-11 03:06:02 +0700

    Update ticket.js

--- languages/en-US/general/ticket.js
+++ languages/en-US/general/ticket.js
@@ -2,14 +2,15 @@
     "DESCRIPTION": "Opens or close a ticket",
     "USAGE": "{{prefix}}ticket [open/close] [reason]",
     "EXAMPLES": "{{prefix}}ticket open report",
-    "DISABLED": "Pong! My ping is `{{ping}}` ms.",
+    "DISABLED": "Ticket system is disabled in this server.",
     "MISSING_ROLE": "Theres some missing role, please contact the staffs here!",
     "MISSING_CHANNEL": "Theres some missing channel, please contact the staffs here!",
     "NO_REASON": "You must specify a reason why you opening a ticket!",
     "LIMIT_CHAR": "Reason must not exceed 20 characters!",
-    "WRONG_CHANNEL": "Go to {{channel}} to open a new ticket!",
+    "OPEN_CHANNEL": "Go to {{channel}} to open a new ticket!",
+    "CLOSE_CHANNEL": "Go to {{channel}} to close your ticket!",
     "RESOLVE_FALSE": "You already have an open ticket here {{channel}}",
     "OPEN": "Your new ticket is opened here {{channel}}",
     "CLOSE": "This is your last ticket transcript.",
-    "EMBED_DESC": "Welcome {{user}}\n\nPlease describe the reasoning for opening this ticket, include any information you think may be relevant such as proof, other third parties and so on.\n\nUse the following command to close the ticket\n`-ticket close [reason]`"
+    "EMBED_DESC": "Welcome {{author}}\n\nPlease describe the reasoning for opening this ticket, include any information you think may be relevant such as proof, other third parties and so on.\n\nUse the following command to close the ticket\n`{{prefix}}ticket close [reason]`"
 }
