97fc3b3b3ff6592aad6cebbb75f86ef12d967d0a
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-13 14:57:19 +0700

    Update takerole.js

--- commands/Moderation/takerole.js
+++ commands/Moderation/takerole.js
@@ -45,15 +45,15 @@ class Takerole extends Command {
             if (!member.roles.cache.has(role.id)) {
                 embed.errorColor()
                 embed.setDescription(emoji.error + " " + message.translate("moderation/takerole:HAS_ROLE", {
-                      member: member.user,
-                      role: role
+                      member: member.user.toString(),
+                      role: role.toString()
                 }));
                 return message.channel.send(embed)
             } else {
                 embed.successColor()
                 embed.setDescription(emoji.success + " " + message.translate("moderation/takerole:SUCCESS", {
-                      member: member.user,
-                      role: role
+                      member: member.user.toString(),
+                      role: role.toString()
                 }));
                 await member.roles.remove(role.id)
                     .then(() => message.channel.send(embed))
