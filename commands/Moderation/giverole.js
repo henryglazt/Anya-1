d47be10491398cea4610a2084c6ae87b4f5fa7fa
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-13 14:52:21 +0700

    Update giverole.js

--- commands/Moderation/giverole.js
+++ commands/Moderation/giverole.js
@@ -50,7 +50,7 @@ class Giverole extends Command {
                 }));
                 return message.channel.send(embed)
             } else {
-                embed.succesColor()
+                embed.successColor()
                 embed.setDescription(emoji.success + " " + message.translate("moderation/giverole:SUCCESS", {
                       member: member.user,
                       role: role
