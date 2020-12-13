d4cbd902a2127fe3762507d0a1e181025e18c196
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-13 23:41:59 +0700

    Update staff.js

--- commands/General/staff.js
+++ commands/General/staff.js
@@ -22,19 +22,30 @@ class Staff extends Command {
     async run(message, args, data) {
 
         const guild = await message.guild.fetch();
+
         const administrators = guild.members.cache.filter((m) => m.hasPermission("ADMINISTRATOR") && !m.user.bot);
         const moderators = guild.members.cache.filter((m) => !administrators.has(m.id) && m.hasPermission("MANAGE_MESSAGES") && !m.user.bot);
+
         const embed = new MessageEmbed()
             .setColor(data.config.embed.color)
             .setFooter(data.config.embed.footer)
             .setAuthor(message.translate("general/staff:TITLE", {
                 guild: message.guild.name
             }))
-            .addField(message.translate("general/staff:ADMINS"), administrators.size > 0 ? administrators.map((a) => `${this.client.customEmojis.status[a.presence.status]} | ${escapeMarkdown(a.user.tag)}`)
+            .addField(message.translate("general/staff:ADMINS"), administrators.size <= 10 && administrators.size !== 0 ? administrators.map((a) => `${this.client.customEmojis.status[a.presence.status]} | ${escapeMarkdown(a.user.tag)}`)
                     .join("\n") : message.translate("general/staff:NO_ADMINS"))
-            .addField(message.translate("general/staff:MODS"), moderators.size > 0 ? moderators.map((m) => `${this.client.customEmojis.status[m.presence.status]} | ${escapeMarkdown(m.user.tag)}`)
-                    .join("\n") : message.translate("general/staff:NO_MODS"))
-        message.channel.send(embed);
+            .addField(message.translate("general/staff:MODS"), moderators.size <= 10 && moderators.size !== 0 ? moderators.map((m) => `${this.client.customEmojis.status[m.presence.status]} | ${escapeMarkdown(m.user.tag)}`).slice(0, 10)
+                    .join("\n") : message.translate("general/staff:NO_MODS"));
+
+        if (administrators.size > 10 && administrators.size <= 20) {
+            embed.addField(message.translate("general/staff:ADMINS"), administrators.size > 10 && administrators.size <= 20 ? administrators.map((m) => `${this.client.customEmojis.status[m.presence.status]} | ${escapeMarkdown(m.user.tag)}`).slice(10, 20).join("\n"));
+        }
+
+        if (moderators.size > 10 && moderators.size <= 20) {
+            embed.addField(message.translate("general/staff:MODS"), moderators.size > 10 && moderators.size <= 20 ? moderators.map((m) => `${this.client.customEmojis.status[m.presence.status]} | ${escapeMarkdown(m.user.tag)}`).slice(10, 20).join("\n"));
+        }
+
+        return message.channel.send(embed);
 
     }
 
