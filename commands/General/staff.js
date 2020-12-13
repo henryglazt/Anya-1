5d5a9f00df57df47cdeafbc9ffeb7dbd90e35ca7
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-14 01:10:34 +0700

    Update staff.js

--- commands/General/staff.js
+++ commands/General/staff.js
@@ -26,16 +26,39 @@ class Staff extends Command {
         const administrators = guild.members.cache.filter((m) => m.hasPermission("ADMINISTRATOR") && !m.user.bot);
         const moderators = guild.members.cache.filter((m) => !administrators.has(m.id) && m.hasPermission("MANAGE_MESSAGES") && !m.user.bot);
 
+        let admin;
+        let mods;
+
+        if (administrators.size > 0) {
+            admin = administrators.map((a) => {
+                let al = a.user.username;
+                if (al.length > 25) al = al.substring(0, 25);
+                `${this.client.customEmojis.status[a.presence.status]} | ${escapeMarkdown(al)} ${a.user.discriminator}`
+            }).join("\n");
+        } else {
+            message.translate("general/staff:NO_ADMINS");
+        };
+
+        if (moderators.size > 0) {
+            mods = moderators.map((a) => {
+                let ml = m.user.username;
+                if (ml.length > 25) ml = ml.substring(0, 25);
+                `${this.client.customEmojis.status[m.presence.status]} | ${escapeMarkdown(ml)} ${m.user.discriminator}`
+            }).join("\n");
+        } else {
+            message.translate("general/staff:NO_MODS");
+        };
+
         const embed = new MessageEmbed()
             .setColor(data.config.embed.color)
             .setFooter(data.config.embed.footer)
             .setAuthor(message.translate("general/staff:TITLE", {
                 guild: message.guild.name
             }))
-            .setDescription([message.translate("general/staff:ADMINS") + "\n" + administrators.size > 0 ? administrators.map((a) => `${this.client.customEmojis.status[a.presence.status]} | ${a.user.tag.length < 25 ? escapeMarkdown(a.user.tag) : escapeMarkdown(a.user.username.substring(0, 25)) + " " + a.user.discriminator}`)
-                    .join("\n") : message.translate("general/staff:NO_ADMINS") + "\n",
-                message.translate("general/staff:MODS") + "\n" + moderators.size > 0 ? moderators.map((m) => `${this.client.customEmojis.status[m.presence.status]} | ${m.user.tag.length < 25 ? escapeMarkdown(m.user.tag) : escapeMarkdown(m.user.username.substring(0, 25)) + " " + m.user.discriminator}`)
-                    .join("\n") : message.translate("general/staff:NO_MODS")]);
+            .setDescription([
+                `${message.translate("general/staff:ADMINS")} ${admins}`,
+                `${message.translate("general/staff:MODS")} ${mods}
+            ]);
 
         return message.channel.send(embed);
 
