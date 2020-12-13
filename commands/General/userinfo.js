0aee326f3e8c432a3b2bffc13e44b93a72fb50cf
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-13 14:34:07 +0700

    Update userinfo.js

--- commands/General/userinfo.js
+++ commands/General/userinfo.js
@@ -72,13 +72,12 @@ class Userinfo extends Command {
 		if(member){
 		const roles = member.roles.cache
 		.sort((a, b) => b.position - a.position)
-		.map(role => role.toString())
-		.slice(0, -1);
+		.map(role => role.toString());
 			embed.addField(`**❯ ${message.translate("common:MEMBER")}:**`, [
 			`**● ${message.translate("common:NICKNAME")}:** ${member.nickname ? `${escapeMarkdown(member.nickname)}` : `\`${message.translate("common:NONE")}\``}`,
 			`**● ${message.translate("common:HIGHEST")} ${message.translate("common:ROLE")}:** ${roles.length ? roles[0] : `\`${message.translate("common:NONE")}\``}`,
 			`**● ${message.translate("common:HEX")} ${message.translate("common:COLOR")}:** \`${member.displayHexColor}\``,
-			`**● ${message.translate("common:ROLES")} [${roles.length}]:** ${roles.length < 16 && roles.length !== 0 ? roles.join(', ') : roles.length > 15 ? roles.slice(0, 14).join(", ") `\`${message.translate("common:AND_MORE")}\`` : `\`${message.translate("common:NONE")}\``}`,
+			`**● ${message.translate("common:ROLES")} [${roles.length}]:** ${roles.length < 16 && roles.length !== 0 ? roles.slice(0, -1).join(', ') : roles.length > 15 ? roles.slice(0, 14).join(", ") `\`${message.translate("common:AND_MORE")}\`` : `\`${message.translate("common:NONE")}\``}`,
 			`**● ${message.translate("common:JOIN")} ${message.translate("common:DATE")}:** ${message.printDate(member.joinedAt)}`,
 			`**● ${message.translate("common:ACKNOWLEDGEMENTS")}:** ${member.guild.owner.id === member.user.id ? `${message.translate("common:SERVER")} ${message.translate("common:OWNER")}` : member.hasPermission('ADMINISTRATOR') ? `${message.translate("common:SERVER")} ${message.translate("common:ADMINISTRATOR")}` : `\`${message.translate("common:NONE")}\``}`,
 			`\u200b`
