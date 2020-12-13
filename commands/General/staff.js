39ca32b48c6651b5289c42d3ae900cf81789f9df
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-14 01:42:32 +0700

    Update staff.js

--- commands/General/staff.js
+++ commands/General/staff.js
@@ -23,30 +23,32 @@ class Staff extends Command {
 
         const guild = await message.guild.fetch();
 
-        const administrators = guild.members.cache.filter((m) => m.hasPermission("ADMINISTRATOR") && !m.user.bot);
-        const moderators = guild.members.cache.filter((m) => !administrators.has(m.id) && m.hasPermission("MANAGE_MESSAGES") && !m.user.bot);
+        const administrators = await guild.members.cache.filter((m) => m.hasPermission("ADMINISTRATOR") && !m.user.bot);
+        const moderators = await guild.members.cache.filter((m) => !administrators.has(m.id) && m.hasPermission("MANAGE_MESSAGES") && !m.user.bot);
 
-        let admin;
-        let mods;
+        let admin = [];
+        let mod = [];
+        let al;
+        let ml;
 
         if (administrators.size > 0) {
-            admin = administrators.map((a) => {
-                let al = a.user.username;
+            administrators.forEach((a) => {
+                al = a.user.username;
                 if (al.length > 25) al = al.substring(0, 25);
-                `${this.client.customEmojis.status[a.presence.status]} | ${escapeMarkdown(al)} ${a.user.discriminator}`
-            }).join("\n");
+                admin.push(`${this.client.customEmojis.status[a.presence.status]} | ${escapeMarkdown(al)}${a.user.discriminator}\n`);
+            });
         } else {
-            message.translate("general/staff:NO_ADMINS");
+            admin = message.translate("general/staff:NO_ADMINS");
         };
 
         if (moderators.size > 0) {
-            mods = moderators.map((m) => {
-                let ml = m.user.username;
+            moderators.forEach((m) => {
+                ml = m.user.username;
                 if (ml.length > 25) ml = ml.substring(0, 25);
-                `${this.client.customEmojis.status[m.presence.status]} | ${escapeMarkdown(ml)} ${m.user.discriminator}`
-            }).join("\n");
+                mod.push(`${this.client.customEmojis.status[m.presence.status]} | ${escapeMarkdown(ml)}${m.user.discriminator}\n`);
+            });
         } else {
-            message.translate("general/staff:NO_MODS");
+            mod = message.translate("general/staff:NO_MODS");
         };
 
         const embed = new MessageEmbed()
@@ -56,8 +58,8 @@ class Staff extends Command {
                 guild: message.guild.name
             }))
             .setDescription([
-                `${message.translate("general/staff:ADMINS")} ${admin}`,
-                `${message.translate("general/staff:MODS")} ${mods}`
+                `${message.translate("general/staff:ADMINS")}\n${admin}`,
+                `${message.translate("general/staff:MODS")}\n${mod}`
             ]);
 
         return message.channel.send(embed);
