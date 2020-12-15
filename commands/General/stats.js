cdf27f44454048977cd6bfa4e0eefe8879cf6f66
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-15 16:28:17 +0700

    Update stats.js

--- commands/General/stats.js
+++ commands/General/stats.js
@@ -1,8 +1,8 @@
 const Command = require("../../base/Command.js"),
-	Discord = require("discord.js");
-	const ms = require('ms');
-	const os = require('os');
-	const core = os.cpus()[0];
+	Discord = require("discord.js"),
+		ms = require("ms"),
+			os = require("os"),
+				core = os.cpus()[0];
 
 class Stats extends Command {
 
@@ -28,36 +28,35 @@ class Stats extends Command {
 			.setFooter(data.config.embed.footer)
 			.setThumbnail(this.client.user.displayAvatarURL({ dynamic: true }))
 			.setAuthor(message.translate("common:STATS"), "https://tinyurl.com/y4xs3cje")
-			.addField("**❯ General:**", [
-			`**● Username:** ${this.client.user.tag}`,
-			`**● ID:** \`${this.client.user.id}\``,
-			`**● Commands:** ${this.client.commands.size}`,
-			`**● Servers:** ${this.client.guilds.cache.size.toLocaleString()} `,
-			`**● Users:** ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`,
+			.addField(`**❯ ${message.translate("common:GENERAL")}:**`, [
+			`**● ${message.translate("common:USERNAME")}:** ${this.client.user.tag}`,
+			`**● ${message.translate("common:ID")}:** \`${this.client.user.id}\``,
+			`**● ${message.translate("common:COMMANDS")}:** ${this.client.commands.size}`,
+			`**● ${message.translate("common:SERVERS")}:** ${this.client.guilds.cache.size.toLocaleString()} `,
+			`**● ${message.translate("common:USERS")}:** ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString()}`,
 			`**● Node.js:** \`v${process.versions.node}\``,
 			`**● Discord.js:** \`v${Discord.version}\``,
-			`**● Creation Date:** ${message.printDate(this.client.user.createdAt)}`,
+			`**● ${message.translate("common:CREATION")} ${message.translate("common:DATE")}:** ${message.printDate(this.client.user.createdAt)}`,
  			`\u200b`
 			])
     
-			.addField("**❯ System**", [
-		  		`**● Platform:** ${process.platform}`,
-	  			`**● Uptime:** ${ms(os.uptime() * 1000, { long: true })}`,
-	  			`**● CPU:**`,
-	  			`> **Cores:** \`${os.cpus().length}\``,
-  				`> **Model:** \`${core.model}\``,
-  				`> **Speed:** \`${core.speed}MHz\``,
-  				`**● Memory:**`,
-  				`> **Heap Total:** \`${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} MB\``,
-  				`> **Heap Used:** \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\``,
+			.addField(`**❯ ${message.translate("common:SYSTEM")}**`, [
+		  		`**● ${message.translate("common:PLATFORM")}:** ${process.platform}`,
+	  			`**● ${message.translate("common:UPTIME")}:** ${ms(os.uptime() * 1000, { long: true })}`,
+	  			`**● ${message.translate("common:CPU")}:**`,
+	  			`> **${message.translate("common:CORES")}:** \`${os.cpus().length}\``,
+  				`> **${message.translate("common:MODEL")}:** \`${core.model}\``,
+  				`> **${message.translate("common:SPEED")}:** \`${core.speed}MHz\``,
+  				`**● ${message.translate("common:MEMORY")}:**`,
+  				`> **${message.translate("common:HEAP")} ${message.translate("common:TOTAL")}:** \`${(process.memoryUsage().heapTotal / 1024 / 1024).toFixed(2)} MB\``,
+  				`> **${message.translate("common:HEAP:")} ${message.translate("common:USED")}:** \`${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB\``,
 				`\u200b`
 				])
 
 			.addField(this.client.customEmojis.link+" "+message.translate("general/stats:LINKS_TITLE"), message.translate("misc:STATS_FOOTER", {
 			inviteLink: await this.client.generateInvite({permissions: "ADMINISTRATOR"}),
 			supportLink: this.client.config.supportURL
-		})
-		);
+		}));
 		message.channel.send(statsEmbed);
 	}
 }
