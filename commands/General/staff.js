b04335e3a3c5c973cd8e24239eb4453af4a40d04
EwoX07 <68818349+EwoX07@users.noreply.github.com>
2020-12-17 06:33:53 +0700

    Update staff.js

--- commands/General/staff.js
+++ commands/General/staff.js
@@ -57,7 +57,7 @@ class Staff extends Command {
             .setAuthor(message.translate("general/staff:TITLE", {
                 guild: message.guild.name
             }))
-            .setDescription(`${message.translate("general/staff:ADMINS")}\n${admin.join("\n")}`);
+            .setDescription(message.translate("general/staff:ADMINS") + "\n" + admin.join("\n"));
 
 
 //${message.translate("general/staff:MODS")}\n${mod}`);
